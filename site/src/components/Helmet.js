b78831aa7b71db325b61ada6a5cd5ad871404202
Luis Oliveira (xGeeks) <l.oliveira@xgeeks.io>
2020-03-21 01:40:46 +0000

    improvement(helmet): update SEO related info
    [skip ci]

--- site/src/components/Helmet.js
+++ site/src/components/Helmet.js
@@ -30,7 +30,7 @@ const Helmet = ({ theme = {} }) => (
     `}
     render={data => {
       const { name, description, profile } = data.contentfulAbout;
-      const title = `${name} Portfolio`;
+      const title = `${name}`;
 
       return (
         <ReactHelmet htmlAttributes={{ lang: 'en' }}>
