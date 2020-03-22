c3f286910c5c517325a8c67fd4072a82125537e6
Dan Sabin <dan.sabin@gmail.com>
2020-03-21 19:11:36 -0700

    feat(map): updated business description

--- map/src/data/markers.ts
+++ map/src/data/markers.ts
@@ -3,8 +3,8 @@ import {SERVICES} from "./index";
 export const MARKERS: { [key: string]: any } = {
   '1': {
     label: '1',
-    contentTitle: 'REMOVE LLC',
-    contentBody: 'Text description, can be plain text or HTML',
+    contentTitle: 'REMOVE LLC.',
+    contentBody: 'REMOVE is a small business modernizing trash and junk removal services through community outreach and redistribution. We can help provide moving and removal services for families and individuals who need help clearing space in their yards or their home as they "stay in place".',
     services: [SERVICES.aid, SERVICES.mobility],
     contact: {
         phone: '+12135452062',
