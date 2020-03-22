62459867b08fd644ec441a67d344925ebab4429a
Dan Sabin <dan.sabin@gmail.com>
2020-03-21 19:14:01 -0700

    feat(map): updated business description

--- map/src/data/markers.ts
+++ map/src/data/markers.ts
@@ -4,7 +4,7 @@ export const MARKERS: { [key: string]: any } = {
   '1': {
     label: '1',
     contentTitle: 'REMOVE LLC.',
-    contentBody: 'REMOVE is a small business modernizing trash and junk removal services through community outreach and redistribution. We can help provide moving and removal services for families and individuals who need help clearing space in their yards or their home as they "stay in place".',
+    contentBody: 'REMOVE is a small business modernizing trash and junk removal services through community outreach and redistribution. We can help provide moving and removal services for families and individuals who need help clearing space in their yards or their home as they "stay in place". Additionally, any who needs any items (beds, tables, appliances) please reach out as we have many items we can give away free of charge.',
     services: [SERVICES.aid, SERVICES.mobility],
     contact: {
         phone: '+12135452062',
