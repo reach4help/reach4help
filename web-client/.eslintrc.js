7a0f389720a1cea57ffdc6c473d4c955e68c8e4c
João Marques <marques.joaopereira@gmail.com>
2020-03-23 11:06:04 +0000

    feat(eslint): add import rules

--- web-client/.eslintrc.js
+++ web-client/.eslintrc.js
@@ -3,15 +3,34 @@ module.exports = {
     '../.eslintrc-ts.js',
   ],
   parserOptions: {
-    project: 'tsconfig.json',
+    project: './tsconfig.json',
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
   env: {
     browser: true,
     es6: true,
   },
+  plugins: [
+    "import-helpers",
+  ],
+  rules: {
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "import-helpers/order-imports": ["error", {
+      "newlinesBetween": "always",
+      "groups": [
+        "/^@testing-library/",
+        "module",
+        ["parent", "sibling", "index"],
+        "/css$/"
+      ],
+      "alphabetize": { "order": "asc", "ignoreCase": true }
+    }
+    ],
+  }
 };
