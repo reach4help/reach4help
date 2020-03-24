7b0cb4703f64f1bc3f287561773d00dd46272159
rbgoncalves <r.goncalves@xgeeks.io>
2020-03-24 12:49:35 +0000

    feat(translations): add example language change

--- web-client/src/pages/index.tsx
+++ web-client/src/pages/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { getI18n, useTranslation } from 'react-i18next';
 
 import ExampleContainer from '../containers/exampleContainer';
 
 const Index: React.FC = () => {
   const { t } = useTranslation();
 
+  const toggleLanguage = () => {
+    const i18n = getI18n();
+
+    i18n.changeLanguage(i18n.language === 'en' ? 'pt' : 'en');
+  };
+
   return (
     <>
       <h3>{t('welcome')}</h3>
+      <button type="button" onClick={toggleLanguage}>Toggle Language</button>
       <div>
         Reach4Help
         <ExampleContainer />
