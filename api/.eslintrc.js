218cfecfd708ff9f5452a919a55390aa797c7a19
Wilson Rodrigues <w.rodrigues@xgeeks.io>
2020-03-24 11:33:44 +0000

    docs(docs): fix documentation

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
     'prettier/@typescript-eslint',
   ],
   root: true,
