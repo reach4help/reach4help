643727df4c0f81828e773c5f0aa036d28c37edfe
Wilson Rodrigues <w.rodrigues@xgeeks.io>
2020-03-24 11:14:04 +0000

    style(prettier): apply prettier in api project

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'prettier/@typescript-eslint',
   ],
   root: true,
