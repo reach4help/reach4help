34595fddda01db8764e8b46a20118f27f3f1d66e
Sam Lanning <sam@samlanning.com>
2020-03-24 14:19:08 +0000

    feat(map): initial styling for results

--- map/src/components/results.tsx
+++ map/src/components/results.tsx
@@ -13,24 +13,79 @@ const Results = (props: Props) => {
   const { className, results, selectMarkerCallback } = props;
   return (
     <div className={className}>
-      {(results || []).map((result, index) => (
-        <div
-          key={index}
-          className="result"
-          onClick={
-            selectMarkerCallback ? () => selectMarkerCallback(index) : undefined
-          }
-        >
-          <div>{index + 1}</div>
-          <div>{result.contentTitle}</div>
-        </div>
-      ))}
+      <div className="header">{`${(results || []).length} results`}</div>
+      <div className="list">
+        {(results || []).map((result, index) => (
+          <div
+            key={index}
+            className="result"
+            onClick={
+              selectMarkerCallback
+                ? () => selectMarkerCallback(index)
+                : undefined
+            }
+          >
+            <div className="number">{index + 1}</div>
+            <div className="info">
+              {result.loc.description && (
+                <div className="location">{result.loc.description}</div>
+              )}
+              <div className="name">{result.contentTitle}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default styled(Results)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
   width: 400px;
-  overflow-y: scroll;
-  background: #9f9;
+
+  > .header {
+    padding: ${p => p.theme.spacingPx}px;
+    background: ${p => p.theme.bg};
+    border-bottom: ${p => p.theme.borderLight};
+    border-top: ${p => p.theme.borderLight};
+  }
+
+  > .list {
+    overflow-y: scroll;
+    height: 0;
+    flex-grow: 1;
+
+    > .result {
+      color: ${p => p.theme.textColor};
+      border-bottom: ${p => p.theme.borderLight};
+      padding: ${p => p.theme.spacingPx}px;
+      display: flex;
+      align-items: start;
+      font-size: 1rem;
+      cursor: pointer;
+
+      &:hover {
+        background: ${p => p.theme.colors.grayLight3};
+
+        .location {
+          color: ${p => p.theme.textColorLight};
+        }
+      }
+
+      .number {
+        color: ${p => p.theme.colors.red};
+        font-size: 20px;
+        font-weight: 600;
+        margin-right: ${p => p.theme.spacingPx}px;
+      }
+
+      .location {
+        color: ${p => p.theme.textColorExtraLight};
+        font-size: 0.9rem;
+        margin-bottom: ${p => p.theme.spacingPx / 2}px;
+      }
+    }
+  }
 `;
--- map/src/styling/theme.ts
+++ map/src/styling/theme.ts
@@ -1,5 +1,20 @@
+const COLORS = {
+  grayDark: '#333',
+  gray: '#666',
+  grayLight: '#888',
+  grayLight2: '#ddd',
+  grayLight3: '#f6f6f6',
+  red: '#ea4335',
+};
+
 export const THEME = {
   spacingPx: 15,
+  bg: COLORS.grayLight3,
+  borderLight: `1px solid ${COLORS.grayLight2}`,
+  textColor: COLORS.grayDark,
+  textColorLight: COLORS.gray,
+  textColorExtraLight: COLORS.grayLight,
+  colors: COLORS,
 };
 
 export type Theme = typeof THEME;
