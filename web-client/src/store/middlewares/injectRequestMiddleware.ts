f1d9652250384e7e834b1cfc990cebfc93530a05
rbgoncalves <r.goncalves@xgeeks.io>
2020-03-24 22:20:02 +0000

    feat(async-actions): add actions for pending, completed and rejected

--- web-client/src/store/middlewares/injectRequestMiddleware.ts
+++ web-client/src/store/middlewares/injectRequestMiddleware.ts
@@ -1,15 +1,30 @@
 import request from '../../http/Request';
 
-const injectRequestMiddleware = () => (next: Function) => async (action: Record<string, any>) => {
+const injectRequestMiddleware = ({ dispatch }: {dispatch: Function}) => (next: Function) => async (action: Record<string, any>) => {
   if (action.api && typeof action.api === 'function') {
-    const response = await action.api(request, action.payload);
 
-    return next({
-      type: `${action.type}`,
-      payload: response.data,
-      meta: action.payload,
-      api: action.api,
+    dispatch({
+      type: `${action.type}_PENDING`,
+      payload: action.payload,
     });
+
+    try {
+
+      const response = await action.api(request, action.payload);
+
+      return next({
+        type: `${action.type}_COMPLETED`,
+        payload: response.data,
+        meta: action.payload,
+        api: action.api,
+      });
+
+    } catch {
+      return next({
+        type: `${action.type}_REJECTED`,
+        payload: action.payload,
+      });
+    }
   }
   return next(action);
 };
