6810b4d776352b0f34bbb18f2a710321e5a7e691
rbgoncalves <r.goncalves@xgeeks.io>
2020-03-24 11:03:36 +0000

    feat(form-container): add yup validation to the example form

--- web-client/src/containers/exampleFormContainer.tsx
+++ web-client/src/containers/exampleFormContainer.tsx
@@ -1,5 +1,6 @@
 import { useFormik } from 'formik';
 import React from 'react';
+import * as Yup from 'yup';
 
 const ExampleFormContainer: React.FC = () => {
 
@@ -7,10 +8,16 @@ const ExampleFormContainer: React.FC = () => {
     initialValues: {
       email: '',
     },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Required'),
+    }),
     onSubmit: (values: Record<string, any>) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
+
   return (
     <form onSubmit={formik.handleSubmit}>
       <label htmlFor="email">Email
@@ -19,9 +26,13 @@ const ExampleFormContainer: React.FC = () => {
           name="email"
           type="email"
           onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
           value={formik.values.email}
         />
       </label>
+      {formik.touched.email && formik.errors.email && (
+        <div>{formik.errors.email}</div>
+      )}
       <button type="submit">Submit</button>
     </form>
   );
