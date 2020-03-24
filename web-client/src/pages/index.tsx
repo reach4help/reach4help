5578aa9408f3524c85b7ce7cb89f05f4837ce4c1
rbgoncalves <r.goncalves@xgeeks.io>
2020-03-24 12:36:20 +0000

    feat(translations): add example translation

--- web-client/src/pages/index.tsx
+++ web-client/src/pages/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import ExampleContainer from '../containers/exampleContainer';
 
-const Index: React.FC = () => (
-  <div>
-    Reach4Help
-    <ExampleContainer />
-  </div>
-);
+const Index: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <h3>{t('welcome')}</h3>
+      <div>
+        Reach4Help
+        <ExampleContainer />
+      </div>
+    </>
+  );
+};
 
 export default Index;
