378a14519271acd13a37f25a006702979c5a2f26
Dan Sabin <dan.sabin@gmail.com>
2020-03-25 15:25:19 -0700

    feat(api): more adjustments

--- api/src/migrations/1585155151974-Comment.ts
+++ api/src/migrations/1585155151974-Comment.ts
@@ -7,12 +7,12 @@ export class Comment1585155151974 implements MigrationInterface {
     await queryRunner.query(
         `CREATE TABLE "comment"
          (
-             id           BIGSERIAL NOT NULL,
-             commenter_user_id BIGINT    NOT NULL,
-             request_id   BIGINT    NOT NULL,
-             content      TEXT      NOT NULL,
-             created_at   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-             updated_at   TIMESTAMP WITH TIME ZONE
+             id         BIGSERIAL NOT NULL,
+             user_id    BIGINT    NOT NULL,
+             request_id BIGINT    NOT NULL,
+             content    TEXT      NOT NULL,
+             created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+             updated_at TIMESTAMP WITH TIME ZONE
          );`,
     );
   }
