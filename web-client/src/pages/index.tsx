badaee3bd49b56a44f50431472a26b2ed3aba13e
João Marques <marques.joaopereira@gmail.com>
2020-03-26 18:10:33 +0000

    feat(facebook-login): add initial integration of facebook login

--- web-client/src/pages/index.tsx
+++ web-client/src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { Button } from 'antd';
 import React from 'react';
+import FacebookAuth from 'react-facebook-auth';
 import { getI18n, useTranslation } from 'react-i18next';
 
-import ExampleContainer from '../containers/exampleContainer';
+const MyFacebookButton = (props: any) => {
+  const { onClick } = props;
+  return (
+    <Button onClick={onClick}>
+      Login with facebook
+    </Button>
+  );
+};
 
 const Index: React.FC = () => {
   const { t } = useTranslation();
@@ -11,14 +20,21 @@ const Index: React.FC = () => {
 
     i18n.changeLanguage(i18n.language === 'en' ? 'pt' : 'en');
   };
-
+  const authenticate = (res: any) => {
+    alert(JSON.stringify(res));
+  };
   return (
     <>
       <h3>{t('welcome')}</h3>
       <button type="button" onClick={toggleLanguage}>Toggle Language</button>
       <div>
         Reach4Help
-        <ExampleContainer />
+        <MyFacebookButton />
+        <FacebookAuth
+          appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+          callback={authenticate}
+          component={MyFacebookButton}
+        />
       </div>
     </>
   );
