0888e3dd7c9950b046c2bd8bfad9315c01af9d7f
Kurt Harriger <kharriger@atlassian.com>
2020-03-27 15:30:46 -0600

    fix: fix e2e test module resolution

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -6,13 +6,14 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeEach(async (done) => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    done();
   });
 
   it('/ (GET)', () => {
