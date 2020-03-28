2596caa4cd7c1e91d0d6f2e00870481b8b505022
João Marques <marques.joaopereira@gmail.com>
2020-03-28 15:56:22 +0000

    feat(mirage-js): add miragejs

---
+++ web-client/src/http/MockServer.ts
@@ -0,0 +1,20 @@
+import { Server } from 'miragejs';
+
+export const initializeMockServer = () => {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+  // eslint-disable-next-line no-new
+  new Server({
+    routes() {
+      this.urlPrefix = `${process.env.REACT_APP_API_BASE_URL}`;
+      this.namespace = '/api';
+
+      // this route will handle the URL '/api/contacts'
+      this.post('/login', () => ({
+        name: 'Joao',
+      }));
+    },
+  });
+  window.fetch(`${process.env.REACT_APP_API_BASE_URL}/api/login`, { method: 'post' }).then();
+};
