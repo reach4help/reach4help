882db8e050342c37946b9514cd74e837c172b485
Mathieu Deous <mat.deous@gmail.com>
2020-03-29 03:18:19 +0200

    enable middleware

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import {
+  HttpModule,
+  MiddlewareConsumer,
+  Module,
+  NestModule
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { CountriesModule } from './countries/countries.module';
 import { AddressesModule } from './addresses/addresses.module';
+import { CloudflareIpService } from './services/cloudflareip/cloudflare-ip.service';
+import { CloudflareFilterMiddleware } from './middleware/cloudflare-filter/cloudflare-filter.middleware';
 
 @Module({
   imports: [
@@ -12,9 +19,13 @@ import { AddressesModule } from './addresses/addresses.module';
     UsersModule,
     CountriesModule,
     AddressesModule,
+    HttpModule
   ],
   controllers: [],
-  providers: [],
+  providers: [CloudflareIpService],
 })
-export class AppModule {
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(CloudflareFilterMiddleware).forRoutes('');
+  }
 }
