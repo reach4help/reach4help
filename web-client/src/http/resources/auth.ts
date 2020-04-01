378ea634f3713ed07e510a9aacaebae6b1df28b7
Dan Sabin <dan.sabin@gmail.com>
2020-04-01 10:14:29 -0700

    feat(auth): add not about auth fails on localhost using the dev app

--- web-client/src/http/resources/auth.ts
+++ web-client/src/http/resources/auth.ts
@@ -11,6 +11,14 @@ export const login = (request: IHTTPRequest) => {
   return request.execute(config).then((response: any) => response.data);
 };
 
+/*
+
+Note: Facebook enforces HTTPS and does not allow login with insecure hosts.
+When using Facebook in development mode with an http://localhost origin, you need to ensure that development mode is turned on for this Facebook App.
+In addition, sign-in will only be allowed with Facebook test accounts.
+
+*/
+
 export const facebookLoginWithFirebase = async (): Promise<firebase.auth.UserCredential> => {
   const provider = new firebase.auth.FacebookAuthProvider();
   const result = await firebaseAuth.signInWithPopup(provider);
