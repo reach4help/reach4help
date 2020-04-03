d55cf98aa7bbbf197c34dad236cdef0bb99d8284
Dan Sabin <dan.sabin@gmail.com>
2020-04-02 20:06:58 -0700

    chore: make social data more consistent

--- site/src/components/Helmet.js
+++ site/src/components/Helmet.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactHelmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import { graphql, StaticQuery } from 'gatsby';
 import { withTheme } from 'styled-components';
 import PropTypes from 'prop-types';
 
@@ -28,31 +28,46 @@ const Helmet = ({ theme = {} }) => (
       return (
         <ReactHelmet htmlAttributes={{ lang: 'en' }}>
           <meta charSet="utf-8" />
-          <title>{`${title} - ${description}`}</title>
+
+          <title>{`${title}`}</title>
           <meta name="description" content={description} />
-          <link rel="shortcut icon" href={`${siteUrl}/images/logo32.png`} />
           <meta name="theme-color" content={theme.background} />
-          <meta name="image" content={`${siteUrl}/images/logo512.png`} />
+
           <meta itemProp="name" content={title} />
           <meta itemProp="description" content={description} />
-          <meta itemProp="image" content={`${siteUrl}/images/logo512.png`} />
-          <meta name="og:title" content={title} />
-          <meta name="og:description" content={description} />
-          <meta name="og:image" content={`${siteUrl}/images/logo512.png`} />
-          <meta name="og:site_name" content={title} />
-          <meta name="og:locale" content="en_US" />
-          <meta name="og:type" content="website" />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:title" content={title} />
-          <meta name="twitter:description" content={description} />
+          <meta
+            itemProp="image"
+            content={`${siteUrl}/images/swarm_banner_main.png`}
+          />
+
+          <meta property="og:site_name" content="Reach4Help" />
+          <meta property="fb:app_id" content="1626611720838538" />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta
+            property="og:image"
+            content={`${siteUrl}/images/swarm_banner_main.png`}
+          />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:type" content="website" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@Reach4HelpOrg" />
+          <meta name="twitter:creator" content="@Reach4HelpOrg" />
           <meta
             name="twitter:image"
-            content={`${siteUrl}/images/logo512.png`}
+            content={`${siteUrl}/images/swarm_banner_main.png`}
           />
           <meta
             name="twitter:image:src"
-            content={`${siteUrl}/images/logo512.png`}
+            content={`${siteUrl}/images/swarm_banner_main.png`}
           />
+          <meta
+            name="twitter:image:alt"
+            content="Reach 4 Help / Unifying help around the world."
+          />
+
+          <link rel="shortcut icon" href={`${siteUrl}/images/logo32.png`} />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
