582f2750244803d1575f11226a81a938f263bb54
Micael Rodrigues <micaelr95@outlook.pt>
2020-04-09 10:49:55 +0100

    feat(error messages): add error messages to input

--- web-client/src/components/UserDataForm/UserDataForm.tsx
+++ web-client/src/components/UserDataForm/UserDataForm.tsx
@@ -58,6 +58,7 @@ const UserDataForm: React.FC<NewRequestProps> = ({
           rules={[
             {
               required: true,
+              message: t('user_data_form.full_name_error_message'),
             },
           ]}
           label={t('full_name')}
@@ -83,6 +84,7 @@ const UserDataForm: React.FC<NewRequestProps> = ({
           rules={[
             {
               required: true,
+              message: t('user_data_form.address_error_message'),
             },
           ]}
           label={t('address')}
@@ -94,6 +96,7 @@ const UserDataForm: React.FC<NewRequestProps> = ({
           rules={[
             {
               required: true,
+              message: t('user_data_form.address_error_message'),
             },
           ]}
         >
