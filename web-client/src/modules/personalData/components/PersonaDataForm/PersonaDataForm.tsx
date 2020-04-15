027911fe7a26b5e29bef81afa95b17425ec30d99
Telmo Dias <telmodias@gmail.com>
2020-04-15 04:47:13 +0200

    feat(#383 adding geolocation to personalData): ongoing, partially integrated with redux with support for controlled components in AntD + added geolocation button already functional

--- web-client/src/modules/personalData/components/PersonaDataForm/PersonaDataForm.tsx
+++ web-client/src/modules/personalData/components/PersonaDataForm/PersonaDataForm.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import gpstarget from '../../../../assets/gpstarget.svg';
 
 const { Text } = Typography;
 
@@ -28,6 +29,18 @@ const StyledButton = styled(Button)`
   margin-top: 40px;
 `;
 
+const GPSButton = styled(Button)`
+  background-color: #1890ff;
+  border-radius: 4px;
+  border: none;
+`;
+
+const GPSTarget = styled.img`
+  width: 16px;
+  height: 16px;
+  margin-right: 8px;
+`;
+
 interface NewRequestProps {
   handleFormSubmit: Function;
 }
@@ -39,10 +52,57 @@ const PersonaDataForm: React.FC<NewRequestProps> = ({
   const [form] = Form.useForm();
   const [fullName, setFullName] = useState('');
   const [displayName, setDisplayName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
+  // geolocation
+  const [geolocationAvailabe, setGeoAvailable] = useState<boolean | undefined>(
+    undefined,
+  );
+  const [geolocationAuthorized, setGeoAuthorized] = useState<
+    undefined | boolean
+  >(undefined);
+
+  const handleGetCoords = () => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          setIsLoading(false);
+          setGeoAuthorized(true);
+          const newCoords = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          console.log(newCoords);
+          // dispatch(setUserGeolocationAction(newCoords));
+          // setCoordsExist(true);
+        },
+        GeolocationPositionError => {
+          setGeoAuthorized(false);
+          setIsLoading(false);
+        },
+      );
+    }, 1000);
+  };
+
+  // useEffect(() => {
+  //   setDisplayName(words(fullName)[0]);
+  // }, [fullName]);
   useEffect(() => {
-    setDisplayName(words(fullName)[0]);
-  }, [fullName]);
+    if (fullName.includes(' ') && displayName === '') {
+      setDisplayName(words(fullName)[0]);
+    }
+    // detect if browser supports geolocation
+    const geoTest = 'geolocation' in navigator;
+    setGeoAvailable(geoTest);
+    // super important to make controlled components in Ant Design
+    form.setFieldsValue({
+      fullName,
+      displayName,
+    });
+  }, [fullName, displayName, form]);
 
   return (
     <StyledIntro>
@@ -57,7 +117,7 @@ const PersonaDataForm: React.FC<NewRequestProps> = ({
         <Row gutter={12}>
           <Col span={24} md={12}>
             <Form.Item
-              name="name"
+              name="fullName"
               rules={[
                 {
                   required: true,
@@ -71,7 +131,6 @@ const PersonaDataForm: React.FC<NewRequestProps> = ({
                   marginRight: '15px',
                 }}
                 placeholder={t('full_name')}
-                value={fullName}
                 onChange={e => setFullName(e.target.value)}
               />
             </Form.Item>
@@ -79,25 +138,36 @@ const PersonaDataForm: React.FC<NewRequestProps> = ({
           <Col span="24" md={12}>
             <Form.Item
               name="displayName"
-              label={t('displayName')}
+              label={t('display_name')}
               rules={[
                 {
                   required: true,
-                  message: t('user_data_form.full_name_error_message'),
+                  message: t('user_data_form.display_name_error_message'),
                 },
               ]}
             >
               <Input
-                placeholder={t('display_name')}
-                value={displayName}
+                placeholder={t('display_name_placeholder')}
                 onChange={e => setDisplayName(e.target.value)}
               />
             </Form.Item>
           </Col>
         </Row>
-
+        {geolocationAuthorized !== false && geolocationAvailabe && (
+          <GPSButton
+            loading={isLoading}
+            icon={<GPSTarget src={gpstarget} />}
+            type="primary"
+            onClick={handleGetCoords}
+          >
+            Use GPS to get my address
+          </GPSButton>
+        )}
+        {geolocationAuthorized === false && (
+          <div>Geolocation not authorized</div>
+        )}
         <Form.Item
-          name="address"
+          name="address1"
           rules={[
             {
               required: true,
@@ -106,7 +176,17 @@ const PersonaDataForm: React.FC<NewRequestProps> = ({
           ]}
           label={t('address')}
         >
-          <Input placeholder={t('address')} />
+          <Input placeholder={t('address1')} />
+        </Form.Item>
+        <Form.Item
+          name="address2"
+          rules={[
+            {
+              required: false,
+            },
+          ]}
+        >
+          <Input placeholder={t('address2')} />
         </Form.Item>
         <Row gutter={12}>
           <Col span={24} md={12}>
@@ -126,7 +206,7 @@ const PersonaDataForm: React.FC<NewRequestProps> = ({
               />
             </Form.Item>
           </Col>
-          <Col span={12} md={6}>
+          <Col span={12} md={6} sm={12} xs={24}>
             <Form.Item
               rules={[
                 {
@@ -143,7 +223,7 @@ const PersonaDataForm: React.FC<NewRequestProps> = ({
               />
             </Form.Item>
           </Col>
-          <Col span={12} md={6}>
+          <Col span={12} md={6} sm={12} xs={24}>
             <Form.Item
               rules={[
                 {
@@ -169,6 +249,7 @@ const PersonaDataForm: React.FC<NewRequestProps> = ({
               message: t('user_data_form.terms_conditions_error'),
             },
           ]}
+          valuePropName="checked"
         >
           <Checkbox>
             {t('user_data_form.terms_conditions_text')}
