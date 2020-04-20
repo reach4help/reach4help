0fe1cf2c02fea355d1c24e304e09571875d7927a
Sam Lanning <sam@samlanning.com>
2020-04-20 13:56:15 +0100

    improvement(map): move language meta outside of strings json

---
+++ map/src/i18n/langs/en.ts
@@ -0,0 +1,12 @@
+/* eslint max-len: 0 */
+import { Language } from '../iface';
+import strings from './en.json';
+
+const LANG: Language = {
+  meta: {
+    name: 'English',
+  },
+  strings,
+};
+
+export default LANG;
---
+++ map/src/i18n/langs/ru.ts
@@ -0,0 +1,12 @@
+/* eslint max-len: 0 */
+import { PartialLanguage } from '../iface';
+import strings from './ru.json';
+
+const LANG: PartialLanguage = {
+  meta: {
+    name: 'Русский',
+  },
+  strings,
+};
+
+export default LANG;
