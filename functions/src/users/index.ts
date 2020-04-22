e0d1fac852becc41d3e9f2a956c6f12fa9c1c69d
Dan Sabin <dan.sabin@gmail.com>
2020-04-22 14:55:50 -0700

    fix: should onyl trigger from DB actions

--- functions/src/users/index.ts
+++ functions/src/users/index.ts
@@ -1,17 +1,9 @@
 import * as functions from 'firebase-functions';
 
-import { onCreate, setIsUserPin } from './functions';
+import { onCreate } from './functions';
 
 export const triggerEventsWhenUserIsCreated = functions.firestore
   .document('users/{userId}')
   .onCreate(onCreate);
-
-export const callableSetUserPinClaim = functions.https.onCall((data: { status: boolean }, context) => {
-  return context.auth?.uid ? setIsUserPin(context.auth.uid, data.status || false) : Promise.reject(new Error('Unauthorized'));
-});
-
-export const callableSetUserCavClaim = functions.https.onCall((data: { status: boolean }, context) => {
-  return context.auth?.uid ? setIsUserPin(context.auth.uid, data.status || false) : Promise.reject(new Error('Unauthorized'));
-});
 
 export * from './privilegedInformation';
