2925c3b7d2f72c17fe59f20e7b429a70377fa4c3
Joseph Ashwin Kottapurath <ashwinkjoseph@gmail.com>
2020-04-26 04:05:05 +0530

    feat: added callbacks in roleinfo component and container for component to communicate to conteiner regarding user's choice

--- web-client/src/modules/personalData/containers/RoleInfoContainer/RoleInfoContainer.tsx
+++ web-client/src/modules/personalData/containers/RoleInfoContainer/RoleInfoContainer.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { ApplicationPreference } from 'src/models/users';
 
 import RoleInfo from '../../components/RoleInfo/RoleInfo';
 
-const RoleInfoContainer: React.FC = () => (
-  <>
-    <RoleInfo />
-  </>
-);
+const RoleInfoContainer: React.FC = () => {
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-empty-function
+  const chooseApplicationPreference = (preference: ApplicationPreference) => {};
+
+  return (
+    <>
+      <RoleInfo chooseApplicationPreference={chooseApplicationPreference} />
+    </>
+  );
+};
 
 RoleInfoContainer.propTypes = {};
 
