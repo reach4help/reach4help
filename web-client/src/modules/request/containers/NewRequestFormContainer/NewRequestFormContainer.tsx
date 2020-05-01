9a95440d060fd5321de8bad3411b1fd36781bc88
puzzledbytheweb <pmsfilipe1@gmail.com>
2020-05-01 02:52:30 +0100

    chore(module): revert back to protected route

--- web-client/src/modules/request/containers/NewRequestFormContainer/NewRequestFormContainer.tsx
+++ web-client/src/modules/request/containers/NewRequestFormContainer/NewRequestFormContainer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import NewRequestForm from '../../components/NewRequestForm/NewRequestForm';
+import { NewRequestSuccessLocation } from '../../pages/routes/NewRequestSuccessRoute/constants';
 
 const NewRequestContainer: React.FC = () => {
+  const history = useHistory();
   // eslint-disable-next-line no-unused-vars
   const handleFormSubmit = (values: { body: string; title: string }) => {
-    // console.log("Form Data:", values);
+    setTimeout(() => history.push(NewRequestSuccessLocation.path), 250);
   };
 
   return (
