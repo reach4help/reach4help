9102eb65d7a38b6b7d11496404fd98e365109a76
Sam Lanning <sam@samlanning.com>
2020-05-05 14:08:53 +0100

    chore(backend): add json content-type to api

--- functions/src/https/api/map/index.ts
+++ functions/src/https/api/map/index.ts
@@ -43,5 +43,6 @@ export const data = functions.https.onRequest(async (_req, res) => {
       });
     }
   });
+  res.setHeader('Content-Type', 'application/json');
   res.status(200).send(JSON.stringify(result));
 });
