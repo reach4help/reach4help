986974c3b586d4e024ea020e0cdd1e43f41f4ba7
Fu-Lin Liu <fulin426@gmail.com>
2020-05-11 19:22:29 -0700

    match css styling according to mockup

--- web-client/src/components/UnsupportedLocation/UnsupportedLocation.tsx
+++ web-client/src/components/UnsupportedLocation/UnsupportedLocation.tsx
@@ -1,31 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Typography } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 import mapPinLocation from '../../assets/mappinlocation.gif';
 
-const { Title, Paragraph } = Typography;
+const { Paragraph } = Typography;
+
+const Container = styled.div`
+  min-height: 585px;
+`;
 
 const MapPinLocation = styled.img`
   height: 162px;
   width: 162px;
+  display: block;
+  margin: auto;
 `;
 
-const StyledTitle = styled(Title)`
+const StyledTitle = styled.h5`
+  font-size: 20px;
   margin-top: 32px;
+  text-align: center;
 `;
 
 const Explanation = styled(Paragraph)`
   margin-top: 34px;
+  text-align: center;
 `;
 
 const UnsupportedLocation = () => {
+  const { t } = useTranslation();
+
   return (
-    <div>
+    <Container>
       <MapPinLocation src={mapPinLocation} alt="map pin location" />
-      <StyledTitle>Title Here</StyledTitle>
-      <Explanation>Info Here</Explanation>
-    </div>
+      <StyledTitle>
+        {t('user_data_form.geolocation_unavailable_title')}
+      </StyledTitle>
+      <Explanation>
+        {t('user_data_form.geolocation_unavailable_text')}
+      </Explanation>
+    </Container>
   );
 };
 
