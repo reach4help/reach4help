5eb8de1353f479edc49dd616df2b823dfc90cd4e
Burhan Tuerker <burhan.tuerker@gmail.com>
2020-05-13 03:15:30 +0300

    improvement(timeline): add mock props regarding meeting discussion, some refactoring

--- web-client/src/components/RequestTimeline/RequestTimeline.tsx
+++ web-client/src/components/RequestTimeline/RequestTimeline.tsx
@@ -1,48 +1,65 @@
-import { StarOutlined } from '@ant-design/icons';
-import { Typography } from 'antd';
-import React, { useState } from 'react';
+import React from 'react';
+import { OfferStatus } from 'src/models/offers';
+import { RequestStatus } from 'src/models/requests';
+import { ApplicationPreference } from 'src/models/users';
 import styled from 'styled-components';
 
-import RatingSlider from '../RatingSlider/RatingSlider';
 import RequestTimelineList from '../RequestTimelineList/RequestTimelineList';
 
-const { Text } = Typography;
+const today = new Date('12/31/2015');
 
-const RequestTimeline: React.FC<RequestTimelineProps> = () => {
-  const [rating, setRating] = useState<number | [number, number]>(0);
-  const handleRatingSubmit = () => alert(`Rating submitted: ${rating}`);
-
-  return (
-    <Wrapper>
-      <Title>Request Timeline</Title>
-      <RequestTimelineList items={[{ align: 'left' }, { align: 'right' }]} />
-      <Actions>
-        <>
-          <RatingMessage>
-            {/* TODO use translation */}
-            How would you rate your experience with Daniel Wade?
-          </RatingMessage>
-          <RatingSlider
-            selected={rating}
-            onChangeHandler={value => setRating(value)}
-          />
-          <ActionButtons>
-            <ActionButton className="btn-green" onClick={handleRatingSubmit}>
-              <StarOutlined />
-              Submit Rating
-            </ActionButton>
-          </ActionButtons>
-          {/* <ActionButton onClick={() => alert('cancel request')}> */}
-          {/*  Cancel Request */}
-          {/* </ActionButton> */}
-          {/* <ActionButton onClick={() => alert('finish request')}> */}
-          {/*  Finish Request */}
-          {/* </ActionButton> */}
-        </>
-      </Actions>
-    </Wrapper>
-  );
+const mockPin = {
+  username: 'PinUser',
+  applicationPreference: ApplicationPreference.pin,
 };
+
+const mockCav = {
+  username: 'CavUser',
+  applicationPreference: ApplicationPreference.cav,
+};
+
+const mockRequest = {
+  pin: mockPin,
+  cav: null,
+  status: RequestStatus.pending,
+};
+
+const mockOffer = {
+  pin: mockPin,
+  request: mockRequest,
+  status: OfferStatus.pending,
+};
+
+const mockTimelineItems = [
+  {
+    type: 'REQUEST_CREATED',
+    user: mockPin,
+    offer: null,
+    request: mockRequest,
+    createdAt: new Date(today).setDate(today.getDate() - 5),
+  },
+  {
+    type: 'OFFER_CREATED',
+    user: mockCav,
+    request: mockRequest,
+    offer: mockOffer,
+    createdAt: new Date(today).setDate(today.getDate() - 4),
+  },
+  {
+    type: 'OFFER_ACCEPTED',
+    user: mockPin,
+    request: { ...mockRequest, status: RequestStatus.ongoing },
+    offer: { ...mockOffer, status: OfferStatus.accepted },
+    createdAt: new Date(today).setDate(today.getDate() - 3),
+  },
+];
+
+const RequestTimeline: React.FC<RequestTimelineProps> = () => (
+  <Wrapper>
+    <Title>Request Timeline</Title>
+    <RequestTimelineList items={mockTimelineItems} currentUser={mockPin} />
+  </Wrapper>
+);
 
 const Wrapper = styled.div`
   display: flex;
@@ -55,47 +72,6 @@ const Wrapper = styled.div`
 
 const Title = styled.h1`
   font-size: 1.2rem;
-`;
-
-const RatingMessage = styled(Text)``;
-
-const Actions = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: auto;
-`;
-
-const ActionButtons = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  margin-top: auto;
-
-  button {
-    border: 1px solid #d9d9d9;
-    background: white;
-    outline: none;
-
-    svg {
-      margin-right: 5px;
-      font-size: 0.8rem;
-    }
-  }
-
-  .btn-green {
-    border: none;
-    color: white;
-    background: #52c41a;
-  }
-`;
-
-const ActionButton = styled.button`
-  width: 100%;
-  max-width: 150px;
-  padding: 10px;
-  margin: 0 10px;
-  border-radius: 4px;
 `;
 
 interface RequestTimelineProps {
--- web-client/src/components/RequestTimelineList/RequestTimelineList.tsx
+++ web-client/src/components/RequestTimelineList/RequestTimelineList.tsx
@@ -1,106 +1,123 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-const renderMessageBox = (align, text) => {
-  const textAlign = align === 'left' ? 'right' : 'left';
-  const float = align === 'left' ? 'right' : 'left';
-  const marginLeft = align === 'left' ? '0' : 'auto';
+import { ApplicationPreference } from '../../models/users';
+
+const RequestTimelineListItem: React.FC<RequestTimelineListItemProps> = ({
+  item,
+  align,
+}) => {
+  const isCav = item.user.applicationPreference === ApplicationPreference.cav;
+
+  const date = new Date(item.createdAt).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'short',
+  });
 
   return (
     <>
-      {align === 'left' ? (
-        <MessagePin style={{ textAlign, float, marginLeft }}>
-          {align === 'left' ? <PointerPin /> : <PointerCav />}
-          {text}
-        </MessagePin>
-      ) : (
-        <MessageCav style={{ textAlign, float }}>
-          {align === 'left' ? <PointerPin /> : <PointerCav />}
-          {text}
-        </MessageCav>
-      )}
+      <Header>{date}</Header>
+      <StyledListItem className={align}>
+        {isCav ? (
+          <CavBullet className="bullet" />
+        ) : (
+          <PinBullet className="bullet" />
+        )}
+        <MessageBox className={`message-box ${isCav ? 'cav' : 'pin'}`}>
+          Cav request itemCav request item
+        </MessageBox>
+      </StyledListItem>
     </>
   );
 };
 
-const RequestTimelineListItem: React.FC<RequestTimelineListItemProps> = ({
-  item,
+const RequestTimelineList: React.FC<RequestTimelineListProps> = ({
+  items,
+  currentUser,
 }) => (
-  <>
-    <p
-      style={{
-        background: '#f0f2f5',
-        zIndex: 1,
-        textAlign: 'center',
-        margin: 0,
-        fontSize: '0.8rem',
-        color: 'rgba(0, 0, 0, 0.45)',
-      }}
-    >
-      30 May 2020
-    </p>
-    <li
-      style={{
-        marginLeft: item?.align === 'left' ? '0' : 'auto',
-        width: '50%',
-      }}
-    >
-      {renderMessageBox(item?.align, 'Blanche created this request')}
-    </li>
-  </>
-);
-
-const RequestTimelineList: React.FC<RequestTimelineListProps> = ({ items }) => (
   <StyledList>
     <Line />
     {items.map((item, index) => (
-      <RequestTimelineListItem key={index} item={item} />
+      <RequestTimelineListItem
+        key={index}
+        item={item}
+        align={item.user === currentUser ? 'left' : 'right'}
+      />
     ))}
   </StyledList>
 );
 
-const Pointer = css`
+const StyledListItem = styled.li`
+  position: relative;
+  width: 50%;
+  margin-left: 0;
+  display: inline-block;
+
+  &.left {
+    margin-left: 0;
+
+    .bullet {
+      right: -7px;
+    }
+
+    .message-box {
+      text-align: right;
+    }
+  }
+
+  &.right {
+    margin-left: auto;
+
+    .bullet {
+      left: -7px;
+    }
+  }
+`;
+
+const Header = styled.div`
+  background: #f0f2f5;
+  z-index: 1;
+  text-align: center;
+  margin: 0;
+  font-size: 0.8rem;
+  color: rgba(0, 0, 0, 0.45);
+`;
+
+const Bullet = css`
   position: absolute;
-  top: 12px;
+  top: 20px;
+`;
+
+const PinBullet = styled.span`
+  ${Bullet}
+  display: inline-block;
   width: 14px;
   height: 14px;
   border-radius: 14px;
-`;
-
-// TODO use global colors
-const PointerPin = styled.span`
-  ${Pointer}
-  // -margin-right - markerW / 2 = -10 - 7
-  right: -17px;
   background: red;
 `;
 
-// TODO use global colors
-const PointerCav = styled.span`
-  ${Pointer}
-  // -margin-left - markerW / 2 = -10 - 7
-  left: -17px;
+const CavBullet = styled.span`
+  ${Bullet}
+  display: inline-block;
+  width: 14px;
+  height: 14px;
+  border-radius: 14px;
   background: blue;
 `;
 
-const MessageBox = css`
-  position: relative;
+const MessageBox = styled.div`
   padding: 5px 7px;
   margin: 10px;
-  display: inline-block;
   border-radius: 4px;
-`;
 
-// TODO use global colors
-const MessagePin = styled.div`
-  ${MessageBox}
-  background: rgba(255, 123, 2, 0.05);
-`;
+  &.pin {
+    background: rgba(255, 123, 2, 0.05);
+  }
 
-// TODO use global colors
-const MessageCav = styled.div`
-  ${MessageBox}
-  background: rgba(129, 30, 120, 0.05);
+  &.cav {
+    background: rgba(129, 30, 120, 0.05);
+  }
 `;
 
 const StyledList = styled.ul`
@@ -125,10 +142,12 @@ const Line = styled.div`
 
 interface RequestTimelineListProps {
   items: any;
+  currentUser: any;
 }
 
 interface RequestTimelineListItemProps {
   item: any;
+  align: 'left' | 'right';
 }
 
 export default RequestTimelineList;
