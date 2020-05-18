25d159cbd30321603b596c6780c70c2771a38e9a
Sam Lanning <sam@samlanning.com>
2020-05-18 15:19:28 +0100

    improvement(model): use io-ts to define models instead of classes
    io-ts is a really cool library that allows us to defined both
    static types, and data validation in a single place, rather than
    having to define them separately using interfaces and
    class-validator or our own custom type-guard functions.
    
    This dramatically reduces the amount of code we need to write for
    each document type, and guarantees that our type-checking
    functions / type-guards work correctly.

--- functions/src/roles/functions.ts
+++ functions/src/roles/functions.ts
@@ -1,13 +1,11 @@
 import { Change, EventContext } from 'firebase-functions/lib/cloud-functions';
 import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
-import * as admin from 'firebase-admin';
+import { firestore } from 'firebase-admin';
 
 import { auth, db } from '../app';
-import { DocumentReference } from '../shared/model/util'
-import { ROLE_AUDITS_COLLECTION_ID, IRoleAudit } from '../shared/model/role-audits';
-import { Role, ROLE_PERMISSION_GROUPS } from '../shared/model/roles';
-import DocumentData = admin.firestore.DocumentData;
-import Timestamp = admin.firestore.Timestamp;
+import { DocumentReference } from '../shared/model/util';
+import { ROLE_AUDITS_COLLECTION_ID, RoleAudit } from '../shared/model/role-audits';
+import { ROLE_PERMISSION_GROUPS, RoleCodec } from '../shared/model/roles';
 
 /**
  * When a role is created/updated/deleted log it and update the target user's claims
@@ -16,10 +14,14 @@ import Timestamp = admin.firestore.Timestamp;
  * @param context
  */
 export const onWrite = (change: Change<DocumentSnapshot>, context: EventContext) => {
-  const before = change.before.data() as Role;
-  const after = change.after.data() as Role;
+  const before = change.before.data();
+  const after = change.after.data();
+  if ((before && !RoleCodec.is(before)) || (after && !RoleCodec.is(after))) {
+    console.error('Invalid role detected: ', change.after.ref);
+    return;
+  }
   const { userId } = context.params;
-  let actingUserRef: DocumentReference<DocumentData> | null = null;
+  let actingUserRef: DocumentReference<unknown> | null = null;
 
   switch (context.authType) {
     case 'USER':
@@ -39,36 +41,34 @@ export const onWrite = (change: Change<DocumentSnapshot>, context: EventContext)
 
   if (after) {
     // go over all claims and set them
-    const afterObject = after.toObject();
     ROLE_PERMISSION_GROUPS.forEach(permissionGroup => {
-      (afterObject[permissionGroup]).forEach(permission => {
+      after[permissionGroup].forEach(permission => {
         newUserClaims[`${permissionGroup}.${permission}`] = true;
       });
     });
   }
 
-  const updateUserClaims = auth?.setCustomUserClaims(userId, null) // Clear old claims
+  const updateUserClaims = auth
+    ?.setCustomUserClaims(userId, null) // Clear old claims
     .then(() => {
       return auth?.setCustomUserClaims(userId, newUserClaims); // Set new claims
     })
     .catch(error => console.error('User Claims Failed: ', error));
 
-  const auditLog: IRoleAudit = {
+  const auditLog: RoleAudit = {
     affectedUserRef: db.collection('users').doc(userId),
     actingUserRef,
-    previousRole: before,
-    currentRole: after,
-    updatedAt: Timestamp.fromDate(new Date(context.timestamp)),
+    previousRole: before || null,
+    currentRole: after || null,
+    updatedAt: firestore.Timestamp.fromDate(new Date(context.timestamp)),
   };
 
-  const addAuditLog = db.collection(ROLE_AUDITS_COLLECTION_ID)
+  const addAuditLog = db
+    .collection(ROLE_AUDITS_COLLECTION_ID)
     .add(auditLog)
     .catch(error => console.error('Audit Log Failed: ', error)); // Fail silently
 
-  const operations = [
-    addAuditLog,
-    updateUserClaims,
-  ];
+  const operations = [addAuditLog, updateUserClaims];
 
   return Promise.all(operations);
 };
--- model/src/util/index.ts
+++ model/src/util/index.ts
@@ -1,8 +1,44 @@
-import { DocumentReference as AdminDocumentReference } from '@google-cloud/firestore';
+import {
+  DocumentReference as AdminDocumentReference,
+  Timestamp as AdminTimestamp,
+} from '@google-cloud/firestore';
 import { firestore } from 'firebase';
+import * as t from 'io-ts';
 
 import FrontendDocumentReference = firestore.DocumentReference;
+import FrontendTimestamp = firestore.Timestamp;
 
 export type DocumentReference<T> =
   | FrontendDocumentReference<T>
   | AdminDocumentReference<T>;
+
+const isDocumentReference = <T>(v: unknown): v is DocumentReference<T> =>
+  v instanceof FrontendDocumentReference || v instanceof AdminDocumentReference;
+
+/**
+ * TODO: extend this with the ability to tie this type to a specific document
+ * type, checking that the path of the document ref is as expected.
+ */
+export const DocumentReferenceCodec = <T>() =>
+  new t.Type<DocumentReference<T>, DocumentReference<T>, unknown>(
+    'DocumentReference',
+    isDocumentReference,
+    (input, context) =>
+      isDocumentReference<T>(input)
+        ? t.success(input)
+        : t.failure(input, context),
+    t.identity,
+  );
+
+export type Timestamp = FrontendTimestamp | AdminTimestamp;
+
+const isTimestamp = (v: unknown): v is Timestamp =>
+  v instanceof FrontendTimestamp || v instanceof AdminTimestamp;
+
+export const TimestampCodec = new t.Type<Timestamp, Timestamp, unknown>(
+  'Timestamp',
+  isTimestamp,
+  (input, context) =>
+    isTimestamp(input) ? t.success(input) : t.failure(input, context),
+  t.identity,
+);
