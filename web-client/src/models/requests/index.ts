4e35782bea8c2bd427f78bccdcb0e1d90f528993
Joseph Ashwin Kottapurath <ashwinkjoseph@gmail.com>
2020-05-22 03:52:01 +0530

    fix: issues related to undefined in applicationPreference type and using streetAddress when creating request in MasterPage.tsx as well

--- web-client/src/models/requests/index.ts
+++ web-client/src/models/requests/index.ts
@@ -35,7 +35,7 @@ export interface IRequest extends firebase.firestore.DocumentData {
   title: string;
   description: string;
   latLng: firebase.firestore.GeoPoint;
-  address: string;
+  streetAddress: string;
   status?: RequestStatus;
   pinRating?: number | null;
   cavRating?: number | null;
@@ -54,7 +54,7 @@ export class Request implements IRequest {
     title: string,
     description: string,
     latLng: firebase.firestore.GeoPoint,
-    address: string,
+    streetAddress: string,
     cavUserRef: firebase.firestore.DocumentReference<
       firebase.firestore.DocumentData
     > | null = null,
@@ -74,7 +74,7 @@ export class Request implements IRequest {
     this._title = title;
     this._description = description;
     this._latLng = latLng;
-    this._address = address;
+    this._streetAddress = streetAddress;
     this._status = status;
     this._createdAt = createdAt;
     this._updatedAt = updatedAt;
@@ -180,14 +180,14 @@ export class Request implements IRequest {
   }
 
   @IsString()
-  private _address: string;
+  private _streetAddress: string;
 
-  get address(): string {
-    return this._address;
+  get streetAddress(): string {
+    return this._streetAddress;
   }
 
-  set address(value: string) {
-    this._address = value;
+  set streetAddress(value: string) {
+    this._streetAddress = value;
   }
 
   @IsEnum(RequestStatus)
@@ -284,7 +284,7 @@ export class Request implements IRequest {
       data.title,
       data.description,
       data.latLng,
-      data.address,
+      data.streetAddress,
       data.cavUserRef,
       // This field may be null
       data.cavUserSnapshot ? User.factory(data.cavUserSnapshot) : null,
@@ -308,7 +308,7 @@ export class Request implements IRequest {
       title: this.title,
       description: this.description,
       latLng: this.latLng,
-      address: this.address,
+      streetAddress: this.streetAddress,
       status: this.status,
       createdAt: this.createdAt.toDate(),
       updatedAt: this.updatedAt.toDate(),
