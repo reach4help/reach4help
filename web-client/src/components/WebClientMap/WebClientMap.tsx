da654b9ae9f1b79010963008c72a3e9875c1ab1f
mcverter <mitchell.verter@gmail.com>
2020-05-22 10:53:30 -0400

    first draft map with geocoding.  need to manage internal state to recenter

--- web-client/src/components/WebClientMap/WebClientMap.tsx
+++ web-client/src/components/WebClientMap/WebClientMap.tsx
@@ -1,7 +1,14 @@
+import { firestore } from 'firebase';
 import GoogleMapReact from 'google-map-react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { RequestMarker, VolunteerMarker } from './WebClientMarker';
+import {
+  RequestMarker,
+  VolunteerMarker,
+  VolunteerMarkerProps,
+} from './WebClientMarker';
+
+const NO_STREET_ADDRESS = 'No Street Address';
 
 declare global {
   interface Window {
@@ -45,32 +52,46 @@ const WebClientMap: React.FC<MapProps> = ({
   const [DirectionsService, setDirectionsService] = useState<any | undefined>(
     undefined,
   );
+  const [center, setCenter] = useState<VolunteerMarkerProps>();
+  const [Geocoder, setGeocoder] = useState<any | undefined>(undefined);
+  const [streetAddress, setStreetAddress] = useState<string>(NO_STREET_ADDRESS);
+  const [doLocationSearch, setDoLocationSearch] = useState<boolean>(false);
+
+  useEffect(() => {
+    function geocodeCallback(result) {
+      if (result && result.length) {
+        console.log(
+          'street address',
+          result[0].geometry.location,
+          result[0].geometry.location.lat(),
+          result[0].geometry.location.lng(),
+          result[0].formatted_address,
+        );
+        //        debugger;
+        //        setCenter(result[0].geometry.location);
+        setStreetAddress(result[0].formatted_address);
+        setDoLocationSearch(false);
+      }
+    }
+    if (!Geocoder) {
+      console.log('no geocoder', volunteerLocation, Geocoder);
+    } else {
+      if (streetAddress === NO_STREET_ADDRESS) {
+        Geocoder.geocode({ location: volunteerLocation }, geocodeCallback);
+      } else {
+        Geocoder.geocode({ address: streetAddress }, geocodeCallback);
+      }
+    }
+  }, [Geocoder, doLocationSearch]);
 
   const initGoogleMapServices = ({ maps }) => {
     if (typeof DirectionsService === 'undefined') {
       setDirectionsService(new maps.DirectionsService());
     }
-  };
-  /*
-  const findDistanceToRequest = id => {
-    const req = requests.find(r => r.id === id);
-    if (req && req.center) {
-      DirectionsService.getDistanceMatrix(
-        {
-          origins: [volunteerLocation],
-          destinations: [req.center],
-          travelMode: 'TRANSIT',
-        },
-        callback,
-      );
-    }
-    function callback(response, status) {
-      // response is an array of travel methods
-      console.log('distance', response, status);
-      debugger;
+    if (typeof Geocoder === 'undefined') {
+      setGeocoder(new maps.Geocoder());
     }
   };
-*/
   const requestClickedHandler = id => {
     setSelectedRequest(id);
     onRequestHandler(id);
@@ -81,28 +102,72 @@ const WebClientMap: React.FC<MapProps> = ({
     return <>Couldnt obtain API KEY</>;
   }
 
+  if (volunteerLocation && !isNaN(volunteerLocation.lat)) {
+    console.log('oooooo center', center);
+    //setCenter(volunteerLocation);
+  }
+  console.log(
+    'center',
+    center,
+    volunteerLocation,
+    !isNaN(volunteerLocation.lat),
+  );
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        yesIWantToUseGoogleMapApiInternals
-        bootstrapURLKeys={{ key: apiKey }}
-        options={createMapOptions}
-        center={volunteerLocation}
-        defaultZoom={zoom}
-        onGoogleApiLoaded={initGoogleMapServices}
+    <>
+      <div
+        style={{
+          opacity: 1,
+          border: '1px solid black',
+          backgroundColor: 'white',
+        }}
       >
-        <VolunteerMarker {...volunteerLocation} />
-        {requests.map(r => (
-          <RequestMarker
-            key={r.id}
-            selected={r.id === selectedRequest}
-            lat={r.center.lat}
-            lng={r.center.lng}
-            onClick={() => requestClickedHandler(r.id)}
+        <div>
+          {' '}
+          Current Address
+          <input
+            onChange={e => {
+              setStreetAddress(e.target.value);
+            }}
+            value={streetAddress}
           />
-        ))}
-      </GoogleMapReact>
-    </div>
+          <div
+            style={{ backgroundColor: 'blue', color: 'white' }}
+            onClick={() => {
+              setDoLocationSearch(true);
+            }}
+          >
+            Search Again
+          </div>
+          <div
+            style={{ backgroundColor: 'green', color: 'white' }}
+            onClick={() => alert(`submitted ${streetAddress}`)}
+          >
+            Submit Address
+          </div>
+        </div>
+      </div>
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          yesIWantToUseGoogleMapApiInternals
+          bootstrapURLKeys={{ key: apiKey }}
+          options={createMapOptions}
+          center={volunteerLocation}
+          defaultZoom={zoom}
+          onGoogleApiLoaded={initGoogleMapServices}
+        >
+          <VolunteerMarker {...volunteerLocation} />
+          {requests.map(r => (
+            <RequestMarker
+              key={r.id}
+              selected={r.id === selectedRequest}
+              lat={r.center.lat}
+              lng={r.center.lng}
+              onClick={() => requestClickedHandler(r.id)}
+            />
+          ))}
+        </GoogleMapReact>
+      </div>
+    </>
   );
 };
 
@@ -123,3 +188,24 @@ interface MapProps {
 }
 
 export default WebClientMap;
+
+/*
+  const findDistanceToRequest = id => {
+    const req = requests.find(r => r.id === id);
+    if (req && req.center) {
+      DirectionsService.getDistanceMatrix(
+        {
+          origins: [volunteerLocation],
+          destinations: [req.center],
+          travelMode: 'TRANSIT',
+        },
+        callback,
+      );
+    }
+    function callback(response, status) {
+      // response is an array of travel methods
+      console.log('distance', response, status);
+      debugger;
+    }
+  };
+*/
