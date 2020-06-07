f0285669249eb15a9c3b19dca54497d461a1e22e
Joseph Ashwin Kottapurath <ashwinkjoseph@gmail.com>
2020-06-07 20:51:58 +0530

    fix: fixes linting and prettier errors for the cloud function

--- functions/src/https/api/search/volunteers.ts
+++ functions/src/https/api/search/volunteers.ts
@@ -6,44 +6,45 @@ import { IUser, User } from '../../../models/users';
 import { IPrivilegedUserInformation, PrivilegedUserInformation } from '../../../models/users/privilegedInformation';
 
 export const getNearbyVolunteers = functions.https.onCall(async (data, context) => {
-    const { lat, lng, radius } = data;
-    const userId = context.auth?.uid;
-  
-    if (userId) {
-      const userRef = db.collection('users').doc(userId);
-      const user = User.factory((await userRef.get()).data() as IUser);
-  
-      if (user && user.applicationPreference) {
-        // ~1 mile of lat and lon in degrees
-        const unitLat = 0.0144927536231884;
-        const unitLng = 0.0181818181818182;
+  const { lat, lng, radius } = data;
+  const userId = context.auth?.uid;
 
-        const lowerLat = lat - unitLat * radius;
-        const lowerLng = lng - unitLng * radius;
+  if (userId) {
+    const userRef = db.collection('users').doc(userId);
+    const user = User.factory((await userRef.get()).data() as IUser);
 
-        const greaterLat = lat + unitLat * radius;
-        const greaterLng = lng + unitLng * radius;
+    if (user && user.applicationPreference) {
+      // ~1 mile of lat and lon in degrees
+      const unitLat = 0.0144927536231884;
+      const unitLng = 0.0181818181818182;
 
-        const lesserGeopoint = new firestore.GeoPoint(lowerLat, lowerLng);
-        const greateGeopoint = new firestore.GeoPoint(greaterLat, greaterLng);
+      const lowerLat = lat - unitLat * radius;
+      const lowerLng = lng - unitLng * radius;
 
-        const queryResults = await db
-          .collectionGroup('privilegedInformation')
-          .where('address.coords', '>=', lesserGeopoint)
-          .where('address.coords', '<=', greateGeopoint)
-          .get();
-        const idToDataMapping: Record<string, IPrivilegedUserInformation> = {};
-        for (const queryResult of queryResults.docs) {
-          idToDataMapping[queryResult.id] = PrivilegedUserInformation.factory(queryResult.data() as IPrivilegedUserInformation).toObject() as IPrivilegedUserInformation;
-        }
-        return {
-          sucess: true,
-          data:idToDataMapping,
-        }
-      } else {
-        throw new functions.https.HttpsError('unauthenticated', "Can't determine the logged in user");
+      const greaterLat = lat + unitLat * radius;
+      const greaterLng = lng + unitLng * radius;
+
+      const lesserGeopoint = new firestore.GeoPoint(lowerLat, lowerLng);
+      const greateGeopoint = new firestore.GeoPoint(greaterLat, greaterLng);
+
+      const queryResults = await db
+        .collectionGroup('privilegedInformation')
+        .where('address.coords', '>=', lesserGeopoint)
+        .where('address.coords', '<=', greateGeopoint)
+        .get();
+      const idToDataMapping: Record<string, IPrivilegedUserInformation> = {};
+      for (const queryResult of queryResults.docs) {
+        idToDataMapping[queryResult.id] = PrivilegedUserInformation.factory(
+          queryResult.data() as IPrivilegedUserInformation,
+        ).toObject() as IPrivilegedUserInformation;
       }
-    } else {
-      throw new functions.https.HttpsError('unauthenticated', "Can't determine the logged in user");
+      return {
+        sucess: true,
+        data: idToDataMapping,
+      };
     }
-  });
+    throw new functions.https.HttpsError('unauthenticated', "Can't determine the logged in user");
+  } else {
+    throw new functions.https.HttpsError('unauthenticated', "Can't determine the logged in user");
+  }
+});
