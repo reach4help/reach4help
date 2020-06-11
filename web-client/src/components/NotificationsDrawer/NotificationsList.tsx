c123ea940da9c27ac4c55f9e602a1373c85e88d2
mcverter <mitchell.verter@gmail.com>
2020-06-11 07:57:57 -0400

    wrap component return to add translation hook

--- web-client/src/components/NotificationsDrawer/NotificationsList.tsx
+++ web-client/src/components/NotificationsDrawer/NotificationsList.tsx
@@ -12,33 +12,36 @@ interface NotificationsList {
 const NotificationsList: React.FC<NotificationsList> = ({
   isCav,
   unseenOffers,
-}): React.ReactElement => (
+}): React.ReactElement => {
   const { t } = useTranslation();
-  <div
-    style={{
-      marginLeft: '15px',
-      marginRight: '15px',
-      color: 'rgba(0, 0, 0, 0.65)',
-      fontFamily: 'Roboto, sans-serif',
-    }}
-  >
-    {unseenOffers.length > 0 &&
-      unseenOffers.map((item, index) => (
-        <Notification
-          key={index}
-          cavUser={item.cavUserSnapshot}
-          offerStatus={item.status}
-          offerRequest={item.requestSnapshot}
-          requestRef={item.requestRef}
-          updatedAt={item.updatedAt.toDate()}
-          isCav={isCav}
-        />
-      ))}
-    {unseenOffers.length === 0 && (
-      <div style={{ textAlign: 'center' }}>
-        <p>{t('components.notification.no_notifications')}</p>
-      </div>
-    )}
-  </div>
-);
+
+  return (
+    <div
+      style={{
+        marginLeft: '15px',
+        marginRight: '15px',
+        color: 'rgba(0, 0, 0, 0.65)',
+        fontFamily: 'Roboto, sans-serif',
+      }}
+    >
+      {unseenOffers.length > 0 &&
+        unseenOffers.map((item, index) => (
+          <Notification
+            key={index}
+            cavUser={item.cavUserSnapshot}
+            offerStatus={item.status}
+            offerRequest={item.requestSnapshot}
+            requestRef={item.requestRef}
+            updatedAt={item.updatedAt.toDate()}
+            isCav={isCav}
+          />
+        ))}
+      {unseenOffers.length === 0 && (
+        <div style={{ textAlign: 'center' }}>
+          <p>{t('components.notification.no_notifications')}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 export default NotificationsList;
