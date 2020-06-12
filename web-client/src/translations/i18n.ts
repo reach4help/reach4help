90db3909c9bae4a9aad621be6e89857bc3e768d2
Joseph Ashwin Kottapurath <ashwinkjoseph@gmail.com>
2020-06-13 04:14:30 +0530

    feat: added french translation file as well in the react 18n config

--- web-client/src/translations/i18n.ts
+++ web-client/src/translations/i18n.ts
@@ -3,6 +3,7 @@ import detector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import en from './en.json';
+import fr from './fr-FR.json';
 import pt from './pt-PT.json';
 
 i18n
@@ -14,6 +15,7 @@ i18n
     resources: {
       en,
       pt,
+      fr,
     },
     ns: ['common'],
     defaultNS: 'common',
