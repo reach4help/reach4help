30095b1e7ede577abb5f252f4963f7c0e5b02cb2
winggo <winggo.tse@gmail.com>
2020-06-29 23:13:56 -0700

    fix: logger and monitoring does not init if tokens not found

--- web-client/src/telemetry/index.ts
+++ web-client/src/telemetry/index.ts
@@ -2,11 +2,13 @@ import { datadogLogs } from '@datadog/browser-logs';
 import { datadogRum } from '@datadog/browser-rum';
 
 export const enableLogger = () => {
-  datadogLogs.init({
-    clientToken: `${process.env.REACT_APP_DATADOG_LOGS_CLIENT_TOKEN}`,
-    forwardErrorsToLogs: true,
-    sampleRate: 100,
-  });
+  if (process.env.REACT_APP_DATADOG_LOGS_CLIENT_TOKEN) {
+    datadogLogs.init({
+      clientToken: `${process.env.REACT_APP_DATADOG_LOGS_CLIENT_TOKEN}`,
+      forwardErrorsToLogs: true,
+      sampleRate: 100,
+    });
+  }
 };
 
 export const logInfo = (message: string, context?: {}) => {
@@ -18,10 +20,13 @@ export const logError = (message: string, context?: {}) => {
 };
 
 export const enableMonitoring = () => {
-  datadogRum.init({
-    applicationId: `${process.env.REACT_APP_DATADOG_APP_ID}`,
-    clientToken: `${process.env.REACT_APP_DATADOG_RUM_CLIENT_TOKEN}`,
-    sampleRate: 100,
-    trackInteractions: true,
-  });
+  if (process.env.REACT_APP_DATADOG_APP_ID &&
+    process.env.REACT_APP_DATADOG_RUM_CLIENT_TOKEN) {
+    datadogRum.init({
+      applicationId: `${process.env.REACT_APP_DATADOG_APP_ID}`,
+      clientToken: `${process.env.REACT_APP_DATADOG_RUM_CLIENT_TOKEN}`,
+      sampleRate: 100,
+      trackInteractions: true,
+    });
+  }
 };
