7792e27fea9f4a587149990a74b05b1d56bc754e
mcverter <mitchell.verter@gmail.com>
2020-07-11 12:38:19 -0400

    adding jest testing

---
+++ jest_setup/create_jest_directories.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+SCRIPT_DIRECTORY=`pwd`
+cd ..
+ROOT_DIRECTORY=`pwd`
+all_directories_file="${SCRIPT_DIRECTORY}/allDirectories.txt"
+jest_basic_file="${SCRIPT_DIRECTORY}/index.jestSample.ts"
+mapfile -t list_of_directories < $all_directories_file
+
+while read dir; do
+    full_path="${ROOT_DIRECTORY}${dir}/__tests__"
+    if [ ! -e $full_path ]
+    then
+	mkdir $full_path
+    fi
+    
+    index_jest="${full_path}/index.ts"
+
+    if [ ! -e $index_jest ]
+    then
+	cp $jest_basic_file $index_jest
+    else
+    cp $jest_basic_file $index_jest   
+    fi    
+done < $all_directories_file
+
+
---
+++ web-client/src/modules/settings/components/SettingsList/__tests__/index.ts
@@ -0,0 +1,9 @@
+/* import React from 'react'; */
+/* import THE_TESTED_COMPONENT from '../THE_TESTED_FILE'; */
+
+describe('foo', () => {
+  /* describe('<THE_TESTED_COMPONENT />', () => { */
+  it('should do the right thing', () => {
+    expect(1).toEqual(1);
+  });
+});
