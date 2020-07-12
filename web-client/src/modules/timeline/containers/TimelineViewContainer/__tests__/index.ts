2fc8df8fd8047d77854e44047fb50decbe5e48a2
mcverter <mitchell.verter@gmail.com>
2020-07-12 12:38:17 -0400

    tests have to be tsx not ts to handle real markup testing

--- web-client/src/modules/timeline/containers/TimelineViewContainer/__tests__/index.ts
+++
@@ -1,9 +0,0 @@
-/* import React from 'react'; */
-/* import THE_TESTED_COMPONENT from '../THE_TESTED_FILE'; */
-
-describe('foo', () => {
-  /* describe('<THE_TESTED_COMPONENT />', () => { */
-  it('should do the right thing', () => {
-    expect(1).toEqual(1);
-  });
-});
