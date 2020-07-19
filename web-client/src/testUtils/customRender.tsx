b73348485d5bf0cce914b0b7b2aef7260dcac34f
Spade, Sean <sspade@axcess-financial.com>
2020-07-19 16:10:04 -0400

    feat: mocks browser 'matchMedia' api

--- web-client/src/testUtils/customRender.tsx
+++ web-client/src/testUtils/customRender.tsx
@@ -7,15 +7,16 @@ import { MemoryRouter } from 'react-router-dom';
 
 import configureStore from '../store';
 import i18n from '../translations/i18n';
+import './matchMediaMock';
 // We are overriding the default render method from 'testing-library', which allows us to mock out
 // redux and other required providers
 const customRender = (ui, options?) => {
   const AllProviders = ({ children }) => (
-    <I18nextProvider i18n={i18n}>
-      <Provider store={configureStore()}>
-        <MemoryRouter>{children}</MemoryRouter>
-      </Provider>
-    </I18nextProvider>
+    <MemoryRouter>
+      <I18nextProvider i18n={i18n}>
+        <Provider store={configureStore()}>{children}</Provider>
+      </I18nextProvider>
+    </MemoryRouter>
   );
   return render(ui, { wrapper: AllProviders, ...options });
 };
