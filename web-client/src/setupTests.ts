ebaafde464d495b0544c0fa5fa6e5bd5533d696f
Joseph Ashwin Kottapurath <ashwinkjoseph@gmail.com>
2020-08-10 21:45:16 +0530

    test: using reac-app-rewired for tests, undoing prev changes

--- web-client/src/setupTests.ts
+++ web-client/src/setupTests.ts
@@ -3,5 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-import 'fake-indexeddb/auto';
-import 'src/testUtils/matchMediaMock';
