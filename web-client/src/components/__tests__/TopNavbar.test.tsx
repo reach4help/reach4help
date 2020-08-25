2650eabd23fb12a7f28ba1f8e87600c2bf7cde56
puzzledbytheweb <pmsfilipe1@gmail.com>
2020-08-25 23:12:25 +0100

    feat: add tests for TopNavbar

---
+++ web-client/src/components/__tests__/TopNavbar.test.tsx
@@ -0,0 +1,57 @@
+import userEvent from '@testing-library/user-event';
+
+import React from 'react';
+import { render, screen } from 'src/testUtils/customRender';
+
+import TopNavbar from '../TopNavbar/TopNavbar';
+
+const mockedProps = {
+  openMenu: jest.fn(),
+  openNotifications: jest.fn(),
+  isCav: true,
+  unseenOffersCount: 0,
+  visible: true,
+};
+
+describe('TopNavbar', () => {
+  it('Opens the Menu when the hamburger button is clicked', async () => {
+    render(<TopNavbar {...mockedProps} />);
+
+    const hamburgerButton = await screen.findByLabelText('Menu Button');
+
+    userEvent.click(hamburgerButton);
+
+    expect(mockedProps.openMenu).toHaveBeenCalledTimes(1);
+  });
+
+  it('Shows a red icon when there are notifications to be seen', async () => {
+    const modifiedProps = { ...mockedProps, unseenOffersCount: 2 };
+    render(<TopNavbar {...modifiedProps} />);
+
+    const notificationsButton = await screen.findByLabelText(
+      'Notifications Button',
+    );
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    expect(notificationsButton.firstChild.style.color).toBe('red');
+  });
+
+  it('Opens the Notifications Page when the bell button is clicked', async () => {
+    render(<TopNavbar {...mockedProps} />);
+
+    const notificationsButton = await screen.findByLabelText(
+      'Notifications Button',
+    );
+
+    userEvent.click(notificationsButton);
+
+    expect(mockedProps.openNotifications).toHaveBeenCalledTimes(1);
+  });
+
+  it("Doesn't render anything when visible is not true", () => {
+    const modifiedProps = { ...mockedProps, visible: false };
+    const { container } = render(<TopNavbar {...modifiedProps} />);
+
+    expect(container.firstChild).toBeNull();
+  });
+});
