b8dbfe4c0f6d7393e6ef5c44ec4454306777a94d
puzzledbytheweb <pmsfilipe1@gmail.com>
2020-09-01 00:21:27 +0100

    Add tests for auth reducer

--- web-client/src/ducks/auth/reducer.ts
+++ web-client/src/ducks/auth/reducer.ts
@@ -16,6 +16,7 @@ const initialState: AuthState = {
   confirmationResult: undefined,
   observerReceivedFirstUpdate: false,
 };
+
 export default createReducer<AuthState>(
   {
     ...facebookReducer,
