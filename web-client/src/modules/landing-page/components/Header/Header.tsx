6ce7cad0101bbd6f6e5fd0646212a726784ecd0f
Warisul Imam <warisulimam@gmail.com>
2020-10-07 21:04:43 +0600

    improvement: completed dektop-layout navbar design

--- web-client/src/modules/landing-page/components/Header/Header.tsx
+++ web-client/src/modules/landing-page/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import betaFlair from '../../assets/beta_flair.svg';
+import langLogo from '../../assets/langLogo.svg';
 import logo from '../../assets/logo.svg';
 import {
   BetaFlair,
   ButtonContainer,
+  Dropdown,
+  Link,
+  LinkContainer,
   LogInButton,
   Logo,
   LogoContainer,
@@ -18,6 +22,23 @@ const Header: React.FC = (): JSX.Element => (
       <Logo src={logo} />
       <BetaFlair src={betaFlair} />
     </LogoContainer>
+
+    <LinkContainer>
+      <Link href="/about">About Us</Link>
+      <Link href="/">Partners</Link>
+      <Link href="/">FAQs</Link>
+      {/* es-lint doesn't allow the use of apostrophe, resolve */}
+      <Link href="/">Mutual Aid Map</Link>
+    </LinkContainer>
+
+    <Dropdown>
+      <Logo src={langLogo} height="25px" />
+      <ul>
+        <li>English</li>
+        <li>Portuguese</li>
+        <li>French</li>
+      </ul>
+    </Dropdown>
 
     <ButtonContainer>
       <LogInButton>Log In</LogInButton>
--- web-client/src/modules/landing-page/components/Header/elements.tsx
+++ web-client/src/modules/landing-page/components/Header/elements.tsx
@@ -20,16 +20,19 @@ export const BetaFlair = styled.img`
 `;
 
 export const LogoContainer = styled.div`
-  margin-left: 5vw;
+  margin-left: 3vw;
 `;
 
-export const ButtonContainer = styled.div``;
+export const ButtonContainer = styled.div`
+  margin-right: 3vw;
+`;
 
 const Button = styled.button`
   height: 35px;
   width: 105px;
   border-radius: 4px;
   margin: 2px;
+  cursor: pointer;
 `;
 
 export const LogInButton = styled(Button)`
@@ -42,6 +45,54 @@ export const SignUpButton = styled(Button)`
   border: none;
   color: white;
   background: #ff7b02;
-  margin-right: 5vw;
   transition: all 0.3s ease;
+`;
+
+export const LinkContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 50vw;
+`;
+
+export const Link = styled.a`
+  text-decoration: none;
+  margin: 0px 15px;
+  color: black;
+`;
+
+export const Dropdown = styled.div`
+  padding-right: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left: 5px;
+  padding-right: 0;
+
+  ul {
+    list-style: none;
+    padding-left: 2px;
+    padding-right: 0;
+    height: 25px;
+    overflow: hidden;
+    position: relative;
+    top: 5px;
+    width: 80px;
+
+    &: hover {
+      position: relative;
+      top: 38px;
+      height: 90px;
+    }
+
+    li {
+      cursor: pointer;
+      border-radius: 5px;
+      padding: 4px;
+      &: hover {
+        color: white;
+        background: #ffcb52;
+      }
+    }
+  }
 `;
