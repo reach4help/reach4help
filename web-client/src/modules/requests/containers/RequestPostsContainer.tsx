79f52fa1baa89ab007a0703b90aac0d233dbbaf6
ethanstrominger <ethanstrominger2@gmail.com>
2020-11-03 05:41:58 -0500

    bug: fixed issue #1021
    Changed RequestPostsContainer and OfferPostsContainer to have
    container logic from PostsContainer.tsx
    
    Modified TabbedPostsPage to set profileState.profile.applicationPreference
    which retriggers query.

--- web-client/src/modules/requests/containers/RequestPostsContainer.tsx
+++ web-client/src/modules/requests/containers/RequestPostsContainer.tsx
@@ -1,10 +1,136 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { ProfileState } from 'src/ducks/profile/types';
+import {
+  getAcceptedRequests,
+  getOpenRequests,
+  resetSetRequestState,
+} from 'src/ducks/requests/actions';
+import { RequestState } from 'src/ducks/requests/types';
+import { ApplicationPreference } from 'src/models/users';
+import { TimelineViewLocation } from 'src/modules/timeline/constants';
 
-import { PostTabsType } from '../constants';
-import PostsContainer from './PostsContainer';
+import LoadingWrapper from '../../../components/LoadingComponent/LoadingComponent';
+import {
+  InformationModal,
+  makeLocalStorageKey,
+} from '../../../components/Modals/OneTimeModal';
+import Header from '../components/Header';
+import RequestItem from '../components/RequestItem';
+import RequestList from '../components/RequestList';
 
-const RequestPostsContainer: React.FC = () => (
-  <PostsContainer postType={PostTabsType.requests} />
-);
+const OpenRequestsContainer: React.FC = () => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-export default RequestPostsContainer;
+  const profileState = useSelector(
+    ({ profile }: { profile: ProfileState }) => profile,
+  );
+  // TODO: console.log('before', profileState.url);
+  // profileState.url = window.location.href;
+  // console.log('after', profileState.url);
+
+  const requestWithOffersAndTimeline = useSelector(
+    ({ requests }: { requests: RequestState }) => {
+      if (
+        profileState.profile?.applicationPreference ===
+        ApplicationPreference.cav
+      ) {
+        return requests.syncAcceptedRequestsState;
+      }
+      return requests.syncOpenRequestsState;
+    },
+  );
+  const pref = profileState.profile?.applicationPreference;
+  useEffect(() => {
+    if (profileState.profile?.applicationPreference === ApplicationPreference.pin) {
+      dispatch(resetSetRequestState());
+    }
+  }, [profileState.profile, pref, dispatch]);
+
+  useEffect(() => {
+    if (profileState.profile && profileState.profile.applicationPreference) {
+      if (
+        profileState.profile.applicationPreference === ApplicationPreference.cav
+      ) {
+        dispatch(
+          getAcceptedRequests({
+            userType: profileState.profile.applicationPreference,
+            userRef: profileState.userRef,
+          }),
+        );
+      } else {
+        dispatch(
+          getOpenRequests({
+            userType: profileState.profile.applicationPreference,
+            userRef: profileState.userRef,
+          }),
+        );
+      }
+    }
+  }, [profileState, pref, dispatch]);
+
+  const handleRequest: Function = id =>
+    history.push(TimelineViewLocation.toUrl({ requestId: id }));
+
+  const toCloseRequest: Function = id => `Fill logic: Remove request ${id}`;
+
+  if (
+    !requestWithOffersAndTimeline.data ||
+    requestWithOffersAndTimeline.loading
+  ) {
+    return <LoadingWrapper />;
+  }
+
+  const instructions = [
+    t('information_modal.OpenRequestsContainer.0'),
+    t('information_modal.OpenRequestsContainer.1'),
+    t('information_modal.OpenRequestsContainer.2'),
+  ];
+  const instructionModalLocalStorageKey = makeLocalStorageKey({
+    prefix: 'reach4help.modalSeen.OpenRequestsContainer',
+    userid: profileState.uid,
+  });
+
+  return (
+    <>
+      <Header
+        requestsType={t(
+          'modules.requests.containers.OpenRequestContainer.open',
+        )}
+        numRequests={
+          Object.keys(requestWithOffersAndTimeline.data || {}).length
+        }
+        isCav={
+          profileState.profile?.applicationPreference ===
+          ApplicationPreference.cav
+        }
+        isAcceptedRequests={false}
+      />
+      <RequestList
+        requests={requestWithOffersAndTimeline.data}
+        loading={
+          requestWithOffersAndTimeline && requestWithOffersAndTimeline.loading
+        }
+        handleRequest={handleRequest}
+        isCavAndOpenRequest={false}
+        isPinAndOpenRequest={
+          profileState.profile?.applicationPreference ===
+          ApplicationPreference.pin
+        }
+        RequestItem={RequestItem}
+        toCloseRequest={toCloseRequest}
+      />
+      <InformationModal
+        title={t('information_modal.OpenRequestsContainer.title')}
+        localStorageKey={instructionModalLocalStorageKey}
+        instructions={instructions}
+      />
+    </>
+  );
+};
+
+export default OpenRequestsContainer;
--- web-client/src/modules/requests/pages/TabbedPostsPage.tsx
+++ web-client/src/modules/requests/pages/TabbedPostsPage.tsx
@@ -1,7 +1,10 @@
 import { Tabs } from 'antd';
 import React, { ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
+import { ProfileState } from 'src/ducks/profile/types';
+import { ApplicationPreference } from 'src/models/users';
 import styled from 'styled-components';
 
 import { MyPostsLocation, PostTabsType } from '../constants';
@@ -31,10 +34,20 @@ const TabbedPosts: React.FC = (): ReactElement => {
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   const { postType } = useParams() as Record<string, string>;
   const { t } = useTranslation();
+  const profileState = useSelector(
+    ({ profile }: { profile: ProfileState }) => profile,
+  );
 
-  function onChange(activeKey: string) {
+  // TODO: how to disable this warning? get error when I try
+  const onChange = (activeKey: string) => {
     history.replace(MyPostsLocation.toUrl({ postType: activeKey }));
-  }
+
+    if (profileState.profile) {
+      profileState.profile.applicationPreference =
+        activeKey === PostTabsType.requests ?
+        ApplicationPreference.pin : ApplicationPreference.cav;
+    }
+  };
 
   return (
     <StyledTabs activeKey={postType} onChange={onChange}>
