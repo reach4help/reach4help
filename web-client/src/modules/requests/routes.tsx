102e0a16336a61341a8b787f3c59e3fe5ec5d38e
Alex Ball <me@alexjball.com>
2020-11-14 12:00:30 -0500

    WIP find requests and algolia

--- web-client/src/modules/requests/routes.tsx
+++ web-client/src/modules/requests/routes.tsx
@@ -5,7 +5,7 @@ import LoadingWrapper from '../../components/LoadingComponent/LoadingComponent';
 import { FindRequestsLocation, MyPostsLocation } from './constants';
 
 const FindRequestsContainer = lazy(() =>
-  import('./containers/FindRequestsContainer'),
+  import('./containers/FindRequestsAlgoliaContainer'),
 );
 
 const TabbedPostsPage = lazy(() => import('./pages/TabbedPostsPage'));
