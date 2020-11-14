81e495173111ad47a03e14079039e97ae4ba790b
14ssharp <43449026+14ssharp@users.noreply.github.com>
2020-11-14 03:13:27 -0700

    feat: reac-69 and reac-70. Implemented the rest of the about page

---
+++ web-client/src/modules/landing-page/components/contributors.tsx
@@ -0,0 +1,90 @@
+// export.<contributor name> = { imgSrc: <img src> }; //
+
+export const StephenSharp = {
+  imgSrc:
+    'https://avatars1.githubusercontent.com/u/43449026?s=400&u=4bbd85e4d109429b45a8830b568c3c67861f17d4&v=4',
+};
+export const AlexVanino = {
+  imgSrc: 'https://avatars2.githubusercontent.com/u/815793?v=4',
+};
+export const EthanStrominger = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/32078396?v=4',
+};
+export const Gorostiaga = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/46466795?v=4',
+};
+export const EstherMalouin = {
+  imgSrc: 'https://avatars3.githubusercontent.com/u/65609627?v=4',
+};
+export const ShayanChowdhury = {
+  imgSrc: 'https://avatars3.githubusercontent.com/u/29135917?v=4',
+};
+export const JuozasGaigalas = {
+  imgSrc: 'https://avatars3.githubusercontent.com/u/4100?v=4',
+};
+export const ThomasHuynh = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/16145617?v=4',
+};
+export const Skspade = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/43147936?v=4',
+};
+export const Sharmmad = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/5454024?v=4',
+};
+export const WinggoTse = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/26425671?v=4',
+};
+export const MitchellVerter = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/1453956?v=4',
+};
+export const DilpreetJohal = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/9207593?s=400',
+};
+export const JosephKottapurath = {
+  imgSrc: 'https://avatars2.githubusercontent.com/u/13694998?v=4',
+};
+export const BurhanTuerker = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/46192266?v=4',
+};
+export const TelmoDias = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/30916?v=4',
+};
+export const RitaSerra = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/62537457?v=4',
+};
+export const PatriciaMiranda = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/62437898?v=4',
+};
+export const Cinthyaejh = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/40286823?v=4',
+};
+export const MicaelRodrigues = {
+  imgSrc: 'https://avatars3.githubusercontent.com/u/12500655?v=4',
+};
+export const NunoAlexandre = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/9502562?v=4',
+};
+export const WilsonRodrigues = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/5550776?v=4',
+};
+export const RubenGoncalves = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/24323690?v=4',
+};
+export const JoaoMarques = {
+  imgSrc: 'https://avatars1.githubusercontent.com/u/52417176?v=4',
+};
+export const DanSabin = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/961844?v=4',
+};
+export const LuisFilipe = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/9373787?v=4',
+};
+export const PedroFilipe = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/35262512?v=4',
+};
+export const SamLanning = {
+  imgSrc: 'https://avatars0.githubusercontent.com/u/3319932?v=4',
+};
+export const DavidAlecrim = {
+  imgSrc: 'https://avatars2.githubusercontent.com/u/5495320?v=4',
+};
