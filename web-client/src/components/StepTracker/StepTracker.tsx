a7e09ca27c3168b0dc9a865d64d799ca6fab8518
mcverter <mitchell.verter@gmail.com>
2020-11-24 18:26:25 -0500

    thematizable colors

--- web-client/src/components/StepTracker/StepTracker.tsx
+++ web-client/src/components/StepTracker/StepTracker.tsx
@@ -12,128 +12,148 @@ const getStatus = (idx, current) => {
   return 'finished';
 };
 
-const StepTracker: React.FC<IStepTracker> = ({ stepTitles, currentStep }) => (
-  <StepColumn>
-    {stepTitles.map((title, idx) => (
-      <StepBox
-        key={idx}
-        idx={idx}
-        stepsQty={stepTitles.length}
-        current={currentStep}
-        title={title}
-      />
-    ))}
-  </StepColumn>
-);
+const StepTracker: React.FC<IStepTracker> = ({
+  stepTitles,
+  currentStep,
+  colors = {
+    backgroundGradient: [COLORS.primaryDark, COLORS.primary],
+    active: COLORS.primaryOrange,
+    waiting: '#ab57a4',
+    finished: '#cc9bc7',
+  },
+}) => {
+  const StepColumn = styled.div`
+    display: flex;
+    flex-direction: row;
+  `;
 
-const StepBox = ({ idx, current, stepsQty, title }) => {
-  const status = getStatus(idx, current);
+  const StepBox = ({ idx, current, stepsQty, title }) => {
+    const status = getStatus(idx, current);
 
+    return (
+      <StepBoxWrapper stepsQty={stepsQty}>
+        <ProgressDiv>
+          <ProgressDot status={status} />
+          {idx >= 1 && (
+            <ProgressConnector stepsQty={stepsQty} status={status} />
+          )}
+        </ProgressDiv>
+        <StepTitle status={status}>{title}</StepTitle>
+      </StepBoxWrapper>
+    );
+  };
+
+  const StepBoxWrapper = styled('div')<IStepBoxWrapper>`
+    height: 98px;
+    width: ${props => `${100 / props.stepsQty}%`};
+    background: radial-gradient(
+      ${colors.backgroundGradient[0]},
+      ${colors.backgroundGradient[1]}
+    );
+  `;
+
+  const StepTitle = styled('div')<IStepTitle>`
+    z-index: 100;
+    position: relative;
+    top: 50%;
+    text-align: center;
+
+    color: ${props =>
+      props.status === 'active' ? COLORS.white : colors.finished}};
+  `;
+
+  const ProgressDiv = styled.div`
+    position: relative;
+    top: 50%;
+    left: 50%;
+    margin-left: -5px;
+  `;
+
+  const ProgressDot = styled('span')<IProgressDot>`
+    width: 10px;
+    height: 10px;
+    z-index: 100;
+    border-radius: 50%;
+    position: absolute;
+    bottom: 5px;
+
+    background: ${props => {
+      switch (props.status) {
+        case 'active':
+          return colors.active;
+        case 'waiting':
+          return colors.waiting;
+        case 'finished':
+          return COLORS.white;
+        default:
+          return colors.waiting;
+      }
+    }};
+  `;
+
+  const ProgressConnector = styled('span')<IProgressConnector>`
+    position: absolute;
+    bottom: 10px;
+    z-index: 100;
+    height: 1px;
+
+    width: ${props => `${(100 * (props.stepsQty - 1)) / props.stepsQty}%`};
+    margin-left: ${props =>
+      `-${(100 * (props.stepsQty - 1) + 100 / (props.stepsQty - 1)) /
+        props.stepsQty}%`};
+    background: ${props => {
+      switch (props.status) {
+        case 'active':
+          return colors.finished;
+        case 'waiting':
+          return colors.waiting;
+        case 'finished':
+          return colors.finished;
+        default:
+          return colors.waiting;
+      }
+    }};
+  `;
   return (
-    <StepBoxWrapper stepsQty={stepsQty}>
-      <ProgressDiv>
-        <ProgressDot status={status} />
-        {idx >= 1 && <ProgressConnector stepsQty={stepsQty} status={status} />}
-      </ProgressDiv>
-      <StepTitle status={status}>{title}</StepTitle>
-    </StepBoxWrapper>
+    <StepColumn>
+      {stepTitles.map((title, idx) => (
+        <StepBox
+          key={idx}
+          idx={idx}
+          stepsQty={stepTitles.length}
+          current={currentStep}
+          title={title}
+        />
+      ))}
+    </StepColumn>
   );
+
+  interface IStepBoxWrapper {
+    stepsQty: number;
+  }
+
+  interface IStepTitle {
+    status: 'active' | 'waiting' | 'finished';
+  }
+
+  interface IProgressDot {
+    status: 'active' | 'waiting' | 'finished';
+  }
+
+  interface IProgressConnector {
+    status: 'active' | 'waiting' | 'finished';
+    stepsQty: number;
+  }
 };
-
-const StepColumn = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const StepBoxWrapper = styled('div')<IStepBoxWrapper>`
-  height: 98px;
-  width: ${props => `${100 / props.stepsQty}%`};
-  background: radial-gradient(${COLORS.primaryDark}, ${COLORS.primary});
-`;
-
-const StepTitle = styled('div')<IStepTitle>`
-  z-index: 100;
-  position: relative;
-  top: 50%;
-  text-align: center;
-
-  color: ${props =>
-    props.status === 'active' ? COLORS.white : COLORS.lightPrimary}};
-`;
-
-const ProgressDiv = styled.div`
-  position: relative;
-  top: 50%;
-  left: 50%;
-  margin-left: -5px;
-`;
-
-const ProgressDot = styled('span')<IProgressDot>`
-  width: 10px;
-  height: 10px;
-  z-index: 100;
-  border-radius: 50%;
-  position: absolute;
-  bottom: 5px;
-
-  background: ${props => {
-    switch (props.status) {
-      case 'active':
-        return COLORS.primaryOrange;
-      case 'waiting':
-        return COLORS.fadedPrimary;
-      case 'finished':
-        return COLORS.white;
-      default:
-        return COLORS.fadedPrimary;
-    }
-  }};
-`;
-
-const ProgressConnector = styled('span')<IProgressConnector>`
-  position: absolute;
-  bottom: 10px;
-  z-index: 100;
-  height: 1px;
-
-  width: ${props => `${(100 * (props.stepsQty - 1)) / props.stepsQty}%`};
-  margin-left: ${props =>
-    `-${(100 * (props.stepsQty - 1) + 100 / (props.stepsQty - 1)) /
-      props.stepsQty}%`};
-  background: ${props => {
-    switch (props.status) {
-      case 'active':
-        return COLORS.lightPrimary;
-      case 'waiting':
-        return COLORS.fadedPrimary;
-      case 'finished':
-        return COLORS.lightPrimary;
-      default:
-        return COLORS.fadedPrimary;
-    }
-  }};
-`;
-
-interface IStepBoxWrapper {
-  stepsQty: number;
-}
-
-interface IStepTitle {
-  status: string;
-}
-
-interface IProgressDot {
-  status: string;
-}
-
-interface IProgressConnector {
-  status: string;
-  stepsQty: number;
-}
-
 export interface IStepTracker {
   stepTitles: string[];
   currentStep: number;
+  colors?: {
+    backgroundGradient: string[];
+    active: string;
+    waiting: string;
+    finished: string;
+  };
 }
 
 export default StepTracker;
