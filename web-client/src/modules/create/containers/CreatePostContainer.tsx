d2baf7cc084b327d7b2031bb66eeab3f7d7177cf
mcverter <mitchell.verter@gmail.com>
2020-12-18 10:06:56 -0500

    refactoring

--- web-client/src/modules/create/containers/CreatePostContainer.tsx
+++ web-client/src/modules/create/containers/CreatePostContainer.tsx
@@ -9,22 +9,42 @@ import { setRequest } from 'src/ducks/requests/actions';
 import { IRequest } from 'src/models/requests';
 import { IUser } from 'src/models/users';
 import { IUserAddress } from 'src/models/users/privilegedInformation';
+import NewAddressModal from 'src/modules/create/components/NewAddressModal';
+import PostDetails from 'src/modules/create/components/PostDetails';
+import PostMap from 'src/modules/create/components/PostMap';
+import PostSummary from 'src/modules/create/components/PostSummary';
 import { MyRequestPostsLocationUrl } from 'src/modules/requests/constants';
 import AuthenticationModal from 'src/pages/modals/AuthenticationModal';
 import { AppState } from 'src/store';
 import styled from 'styled-components';
-
-import PostDetails from '../components/PostDetails';
-import PostMap from '../components/PostMap';
-import PostSummary from '../components/PostSummary';
 
 const CreatePostContainer: React.FC = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { t } = useTranslation();
 
-  const getTypes = () => [t('modules.create.defaults.postDetails.type')];
+  const profileState = useSelector(
+    ({ profile }: { profile: ProfileState }) => profile,
+  );
+  const phoneNumber = useSelector(
+    (state: AppState) => state.auth.user?.phoneNumber,
+  );
+  const onboarded = useSelector((state: AppState) => state.auth.onboarded);
 
+  /* steps */
+  const [stepNumber, setStepNumber] = useState(0);
+  const moveForwards = () => setStepNumber(stepNumber + 1);
+  const moveBackwards = () => setStepNumber(stepNumber - 1);
+
+  /* PostDetails */
+  const getTypes = () => [t('modules.create.defaults.postDetails.type')];
+  const [postDetails, setPostDetails] = useState<IPostDetails>({
+    title: t('modules.create.defaults.postDetails.title'),
+    body: t('modules.create.defaults.postDetails.body'),
+    type: getTypes()[0],
+  });
+
+  /* PostLocation */
   const defaultUserAddress = {
     name: '',
     address1: '',
@@ -35,37 +55,34 @@ const CreatePostContainer: React.FC = () => {
     country: '',
     coords: new firestore.GeoPoint(0, 0),
   };
-
-  const phoneNumber = useSelector(
-    (state: AppState) => state.auth.user?.phoneNumber,
-  );
-
-  const onboarded = useSelector((state: AppState) => state.auth.onboarded);
-  const profileState = useSelector(
-    ({ profile }: { profile: ProfileState }) => profile,
-  );
   const addresses = useSelector(
     (state: AppState) => state.profile.privilegedInformation?.addresses,
   );
-
-  const [stepNumber, setStepNumber] = useState(0);
-  const [postDetails, setPostDetails] = useState<IPostDetails>({
-    title: t('modules.create.defaults.postDetails.title'),
-    body: t('modules.create.defaults.postDetails.body'),
-    type: getTypes()[0],
-  });
-
   const [postLocation, setPostLocation] = useState<IUserAddress>(
     addresses && Object.keys(addresses).length > 0
       ? addresses[Object.keys(addresses)[0]]
       : defaultUserAddress,
   );
 
-  const moveForwards = () => setStepNumber(stepNumber + 1);
-  const moveBackwards = () => setStepNumber(stepNumber - 1);
-  const cancelCreate = () => {
-    history.replace(MyRequestPostsLocationUrl);
+  /* NewAddressModal */
+  const [showNewAddressModal, setShowNewAddressModal] = useState<boolean>(
+    false,
+  );
+  const [geocodeFailed, setGeocodeFailed] = useState<boolean>(false);
+  const onGeocodeFail = () => {
+    setGeocodeFailed(true);
   };
+  const newAddressModalSuccess = value => {
+    setPostLocation(value);
+    setShowNewAddressModal(false);
+    setGeocodeFailed(false);
+  };
+  const closeNewAddressModal = () => {
+    setShowNewAddressModal(false);
+    setGeocodeFailed(false);
+  };
+
+  /* CreatePost */
   const submitPost = () => {
     const { title, body } = postDetails;
     const {
@@ -89,7 +106,10 @@ const CreatePostContainer: React.FC = () => {
       streetAddress: `${address1} ${address2} ${city} ${state} ${postalCode} ${country}`,
       latLng: new firestore.GeoPoint(coords.latitude, coords.longitude),
     };
-    dispatch(setRequest(newPost as IRequest, undefined, phoneNumber));
+    return dispatch(setRequest(newPost as IRequest, undefined, phoneNumber));
+  };
+  const cancelCreate = () => {
+    history.replace(MyRequestPostsLocationUrl);
   };
 
   const postCreationSteps = [
@@ -109,6 +129,7 @@ const CreatePostContainer: React.FC = () => {
       title: t('modules.create.stepTitles.map'),
       component: (
         <PostMap
+          setShowNewAddressModal={setShowNewAddressModal}
           nextHandler={moveForwards}
           prevHandler={moveBackwards}
           postDetails={postDetails}
@@ -125,33 +146,36 @@ const CreatePostContainer: React.FC = () => {
           prevHandler={moveBackwards}
           postDetails={postDetails}
           postLocation={postLocation}
-          submitRequest={submitPost}
+          submitPost={submitPost}
         />
       ),
     },
   ];
 
-  return (
+  return !onboarded ? (
+    <AuthenticationModal isVisible />
+  ) : (
     <CreatePostContainerWrapper>
-      {onboarded ? (
-        <>
-          <StepTracker
-            currentStep={stepNumber}
-            stepTitles={postCreationSteps.map(s => s.title)}
-          />
-          <StepsWrapper>{postCreationSteps[stepNumber].component}</StepsWrapper>
-        </>
-      ) : (
-        <AuthenticationModal isVisible />
-      )}
+      <StepTracker
+        currentStep={stepNumber}
+        stepTitles={postCreationSteps.map(s => s.title)}
+      />
+      <StepsWrapper>{postCreationSteps[stepNumber].component}</StepsWrapper>
+      <NewAddressModal
+        visible={showNewAddressModal}
+        closeModal={closeNewAddressModal}
+        modalSuccess={newAddressModalSuccess}
+        onGeocodeFail={onGeocodeFail}
+        geocodeFailed={geocodeFailed}
+      />
     </CreatePostContainerWrapper>
   );
 };
 
 const StepsWrapper = styled.div`
-  height: 100%;
+  height: calc(100%-98px);
   width: 80%;
-  margin: 20px auto;
+  margin: 0 auto;
 `;
 
 const CreatePostContainerWrapper = styled.div`
