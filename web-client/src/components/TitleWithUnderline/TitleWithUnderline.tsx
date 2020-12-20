a00fd1bdd4c1d4a7b8cf73b3b00a7a78958fdd49
mcverter <mitchell.verter@gmail.com>
2020-12-20 06:27:28 -0500

    Revert "Revert "Feature/create post""
    This reverts commit 648f1ca27c2a82d2cd142b621604491cfed1ba8e.

--- web-client/src/components/TitleWithUnderline/TitleWithUnderline.tsx
+++ web-client/src/components/TitleWithUnderline/TitleWithUnderline.tsx
@@ -4,32 +4,34 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../theme/colors';
 
-const TitleWithOrangeUnderline: React.FC<TitleWithAddonProps> = ({
+const TitleWithUnderline: React.FC<TitleWithAddonProps> = ({
   children,
   level,
-  orangealignment,
+  alignment,
   left,
   bottom,
   transform,
+  color,
 }): React.ReactElement => (
-  <TitleWithOrangeUnderlineOuterDiv>
-    <TitleWithOrangeUnderlineInnerDiv
-      orangealignment={orangealignment}
+  <TitleWithUnderlineOuterDiv>
+    <TitleWithUnderlineInnerDiv
+      alignment={alignment}
       level={level}
       left={left}
       bottom={bottom}
       transform={transform}
+      color={color}
     >
       {children}
-    </TitleWithOrangeUnderlineInnerDiv>
-  </TitleWithOrangeUnderlineOuterDiv>
+    </TitleWithUnderlineInnerDiv>
+  </TitleWithUnderlineOuterDiv>
 );
 
-const TitleWithOrangeUnderlineOuterDiv = styled.div`
+const TitleWithUnderlineOuterDiv = styled.div`
   position: relative;
 `;
 
-const TitleWithOrangeUnderlineInnerDiv = styled(Typography.Title)`
+const TitleWithUnderlineInnerDiv = styled(Typography.Title)`
   white-space: pre-line;
   text-align: ${(props: TitleWithAddonProps) => props.left || 'center'};
   :after {
@@ -38,7 +40,7 @@ const TitleWithOrangeUnderlineInnerDiv = styled(Typography.Title)`
     width: 40px;
     height: 6px;
     left: ${(props: TitleWithAddonProps) => {
-      switch (props.orangealignment) {
+      switch (props.alignment) {
         case 'left':
           return props.left || '12%';
         case 'right':
@@ -52,7 +54,7 @@ const TitleWithOrangeUnderlineInnerDiv = styled(Typography.Title)`
     bottom: ${(props: TitleWithAddonProps) => props.bottom || 0};
     transform: ${(props: TitleWithAddonProps) =>
       props.transform || 'translate(-50%, 0)'};
-    background-color: ${COLORS.highlight};
+    background-color: ${props => props.color || COLORS.highlight};
   }
 `;
 
@@ -60,10 +62,11 @@ interface TitleWithAddonProps {
   children: React.ReactNode;
   level: 1 | 2 | 3 | 4;
   // REVIEW: I need help defining this type
-  orangealignment?: string;
+  alignment?: string;
   left?: string;
   bottom?: string;
   transform?: string;
+  color?: string;
 }
 
-export default TitleWithOrangeUnderline;
+export default TitleWithUnderline;
--- web-client/src/modules/phone/containers/PhoneEntryContainer/PhoneEntryContainer.tsx
+++ web-client/src/modules/phone/containers/PhoneEntryContainer/PhoneEntryContainer.tsx
@@ -2,7 +2,7 @@ import { Alert, Typography } from 'antd';
 import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import TitleWithOrangeUnderline from 'src/components/TitleWithOrangeUnderline/TitleWithOrangeUnderline';
+import TitleWithUnderline from 'src/components/TitleWithUnderline/TitleWithUnderline';
 import { triggerLoginWithPhone } from 'src/ducks/auth/phone/actions';
 import firebase from 'src/firebase';
 import { AppState } from 'src/store';
@@ -50,9 +50,9 @@ const PhoneEntryContainer: React.FC = () => {
       <ErrorMessage>
         {errorMessage && <Alert message={errorMessage} type="error" />}
       </ErrorMessage>
-      <TitleWithOrangeUnderline level={3} orangealignment="center">
+      <TitleWithUnderline level={3} orangealignment="center">
         {t('phoneNumber.title')}
-      </TitleWithOrangeUnderline>
+      </TitleWithUnderline>
       <PhoneNumberEntryForm
         loading={loading}
         reset={error instanceof Error}
