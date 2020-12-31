b8c648082251c59064da2badf8b9362e5e50b064
ethanstrominger <ethanstrominger2@gmail.com>
2020-12-31 06:35:35 -0500

    feat: remove recently added unused exported const

--- web-client/src/models/users/index.ts
+++ web-client/src/models/users/index.ts
@@ -37,7 +37,6 @@ export interface IUser extends firebase.firestore.DocumentData {
   createdAt?: firebase.firestore.Timestamp;
 }
 
-export type UserRefType = firebase.firestore.DocumentReference<User>;
 export class User implements IUser {
   constructor(
     username: string,
