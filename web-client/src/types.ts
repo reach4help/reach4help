ea69b5a97828593fa63828dff5f4bd0ab985fd79
ethanstrominger <ethanstrominger2@gmail.com>
2021-01-08 18:12:41 -0500

    feat: mv files from xgeneralrequests to ..offers

--- web-client/src/types.ts
+++
@@ -1,5 +0,0 @@
-export type DataReferenceType = firebase.firestore.DocumentReference<
-  firebase.firestore.DocumentData
->;
-const idExample: string | null = 'example';
-export type IdType = typeof idExample;
