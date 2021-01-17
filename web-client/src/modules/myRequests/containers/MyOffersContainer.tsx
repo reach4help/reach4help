80b134022f89cd1630088abf16c4513400727a79
ethanstrominger <ethanstrominger2@gmail.com>
2021-01-17 18:53:26 -0500

    feat: create ducks/generalrequest, repl creategeneralrequiest=>creategeneralpost

--- web-client/src/modules/myRequests/containers/MyOffersContainer.tsx
+++ web-client/src/modules/myRequests/containers/MyOffersContainer.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
+import { PostState } from 'src/ducks/GeneralRequests/types';
 import { observeGetMyOffers } from 'src/ducks/myOffers/actions';
-import { PostState } from 'src/ducks/myRequests/types';
 import { ProfileState } from 'src/ducks/profile/types';
 import { ApplicationPreference } from 'src/models/users';
 // TODO: (es) remove import { TimelineViewLocation } from 'src/modules/timeline/constants';
--- web-client/src/modules/myRequests/containers/MyRequestsContainer.tsx
+++ web-client/src/modules/myRequests/containers/MyRequestsContainer.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
-import { observeGetMyRequests } from 'src/ducks/myRequests/actions';
-import { PostState } from 'src/ducks/myRequests/types';
+import { observeGetMyRequests } from 'src/ducks/GeneralRequests/actions';
+import { PostState } from 'src/ducks/GeneralRequests/types';
 import { ProfileState } from 'src/ducks/profile/types';
 import { ApplicationPreference } from 'src/models/users';
 
