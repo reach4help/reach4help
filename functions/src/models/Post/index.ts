5e53b8c481856a7a00d8b980b514556da7def595
ethanstrominger <ethanstrominger2@gmail.com>
2021-01-25 11:57:46 -0500

    feat: fix cloud function index.ts

--- functions/src/models/Post/index.ts
+++ functions/src/models/Post/index.ts
@@ -392,6 +392,9 @@ export class Post implements IPost {
 }
 
 export const PostFirestoreConverter: firebase.firestore.FirestoreDataConverter<Post> = {
-  fromFirestore: (data: firebase.firestore.QueryDocumentSnapshot<IPost>): Post => Post.factory(data.data()),
-  toFirestore: (modelObject: Post): firebase.firestore.DocumentData => modelObject.toObject(),
+  fromFirestore: (
+    data: firebase.firestore.QueryDocumentSnapshot<IPost>,
+  ): Post => Post.factory(data.data()),
+  toFirestore: (modelObject: Post): firebase.firestore.DocumentData =>
+    modelObject.toObject(),
 };
