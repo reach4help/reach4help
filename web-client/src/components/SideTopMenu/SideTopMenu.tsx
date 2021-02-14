2a55c2d1c103a0915e993545a73e3e8564cd850f
Your Name Comes HereMOhamed <mohamedatta447@gmail.com>
2021-02-14 21:33:43 +0200

    made create new in side menu appaer to non logged users

--- web-client/src/components/SideTopMenu/SideTopMenu.tsx
+++ web-client/src/components/SideTopMenu/SideTopMenu.tsx
@@ -1,6 +1,6 @@
-import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
-import React, { useState } from 'react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import {
   CreateOfferLocationUrl,
@@ -31,19 +31,21 @@ const SideMenuLink: React.FC<{
     </StyledMenuItem>
   </Link>
 );
+const { SubMenu } = Menu;
 
 const SideTopMenu: React.FC<{
   closeDrawer: () => void;
   isLoggedIn: boolean;
 }> = ({ closeDrawer, isLoggedIn }) => {
-  const [createNewShowing, setCreateNewShowing] = useState(false);
+  const { t } = useTranslation();
 
   const SideTopMenuStyle = styled('div')`
-  // margin-top: 100px;
-  margin-top: ${!isLoggedIn ? '40px' : '100px'};
-  flex: .65;
+  margin-top: 100px;
+  // margin-top: ${!isLoggedIn ? '40px' : '100px'};
+  flex: .5;
   display: flex;
   flex-direction: column;
+  margin-left: 2rem;
 
   .ant-menu {
     background: ${COLORS.white};
@@ -68,8 +70,10 @@ const SideTopMenu: React.FC<{
 
     .ant-menu-submenu {
       .ant-menu-submenu-title {
+        font-size: 24px;
+        font-weight: 700;
         margin: 0;
-        color: ${COLORS.link};
+        color: inherit;
       }
       .ant-menu-sub {
         background-color: inherit;
@@ -98,44 +102,6 @@ const SideTopMenu: React.FC<{
     transition: 1s ease all;
   `;
 
-  const CreateNew = styled('div')`
-    position: relative;
-    left: 17%;
-    overflow: hidden;
-    height: 44%;
-    z-index: 2;
-    bottom: 5%;
-
-    h4 {
-      position: relative;
-      top: 20%;
-      font-size: 24px;
-      font-weight: 700;
-
-      svg {
-        position: relative;
-        top: 2.5px;
-      }
-    }
-  `;
-
-  const CreateNewMenu = createNewShowing
-    ? styled(StyledMenu)`
-        top: 20%;
-        bottom: 0%;
-      `
-    : styled(StyledMenu)`
-        bottom: 150%;
-      `;
-
-  const AboutSideMenuLink = createNewShowing
-    ? styled(SideMenuLink)`
-        bottom: 20%;
-      `
-    : styled(SideMenuLink)`
-        bottom: 225%;
-      `;
-
   return (
     <SideTopMenuStyle>
       <StyledMenu mode="inline">
@@ -145,50 +111,31 @@ const SideTopMenu: React.FC<{
           path={HomePageLocation.path}
           onClick={closeDrawer}
         />
+        <SideMenuLink
+          key="HelpRequests"
+          title="Help Requests"
+          path={MyOfferPostsLocationUrl}
+          onClick={closeDrawer}
+        />
+        <SideMenuLink
+          key="VolunteerOffers"
+          title="Volunteer Offers"
+          path={MyOfferPostsLocationUrl}
+          onClick={closeDrawer}
+        />
 
-        {true && (
-          <SideMenuLink
-            key="HelpRequests"
-            title="Help Requests"
-            path={MyOfferPostsLocationUrl}
-            onClick={closeDrawer}
-          />
-        )}
-
-        {true && (
-          <SideMenuLink
-            key="VolunteerOffers"
-            title="Volunteer Offers"
-            path={MyOfferPostsLocationUrl}
-            onClick={closeDrawer}
-          />
-        )}
-
-        {isLoggedIn && (
-          <CreateNew
-            onClick={() => setCreateNewShowing(!createNewShowing)}
-            // render me conditionally (when the user's logged in)
-          >
-            <h4>
-              Create New
-              {!createNewShowing ? <CaretDownOutlined /> : <CaretUpOutlined />}
-            </h4>
-            <CreateNewMenu mode="inline">
-              <SideMenuLink
-                key="CreateRequest"
-                title="Create Request"
-                path={CreateRequestLocationUrl}
-                onClick={closeDrawer}
-              />
-              <SideMenuLink
-                key="CreateOffer"
-                title="Create Offer - not implemented"
-                path={CreateOfferLocationUrl}
-                onClick={closeDrawer}
-              />
-            </CreateNewMenu>
-          </CreateNew>
-        )}
+        <SubMenu title="Create New">
+          <Menu.Item>
+            <StyledLink to={CreateRequestLocationUrl}>
+              {t('navbar.create_new.options.request')}
+            </StyledLink>
+          </Menu.Item>
+          <Menu.Item>
+            <StyledLink to={CreateOfferLocationUrl}>
+              {t('navbar.create_new.options.offer')}
+            </StyledLink>
+          </Menu.Item>
+        </SubMenu>
 
         {isLoggedIn && (
           <SideMenuLink
@@ -215,18 +162,17 @@ const SideTopMenu: React.FC<{
             onClick={closeDrawer}
           />
         ) : (
-          <AboutSideMenuLink
+          <SideMenuLink
             key="AboutUs"
             title="About Us"
             path={AboutPageLocation.path}
             onClick={closeDrawer}
           />
         )}
+        <LanguageSelectorContainer>
+          <LanguageSelector />
+        </LanguageSelectorContainer>
       </StyledMenu>
-
-      <LanguageSelectorContainer>
-        <LanguageSelector />
-      </LanguageSelectorContainer>
     </SideTopMenuStyle>
   );
 };
@@ -234,14 +180,23 @@ const SideTopMenu: React.FC<{
 const StyledMenuItem = styled(Menu.Item)`
   font-size: 24px;
   font-weight: 700;
+`;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+  font-size: 16px;
   position: relative;
-  left: 10%;
+
+  :hover {
+    color: ${COLORS.primaryOrange};
+  }
 `;
 
 const LanguageSelectorContainer = styled.div`
-  display: flex;
-  position: relative;
-  bottom: 10%;
+  font-size: 24px;
+  font-weight: 700;
+  margin-left: 0.8rem;
 `;
 
 export default SideTopMenu;
