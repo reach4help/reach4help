441e2f02ee40263a61510691e834f4048792efec
Shayan Chowdhury <shayan@reach4help.org>
2021-03-27 21:31:04 +0600

    fix: build fail caused by loading antd .less config

--- web-client/config-overrides.js
+++ web-client/config-overrides.js
@@ -1,23 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
-
-const { COLORS } = require('./src/theme/colors');
+const { override, fixBabelImports } = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: true,
-  }),
-  addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: {
-      '@primary-color': COLORS.primary,
-      '@link-color': COLORS.link,
-      '@highlight-color': COLORS.highlight,
-      hack: `true;@import "${require.resolve(
-        './src/theme/themeOverrides.less',
-      )}";`,
-    },
   }),
 );
