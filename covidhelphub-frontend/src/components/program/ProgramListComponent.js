ea0b2f00b0f6a775bbee06a9d8f64bf6a7767e69
ethanstrominger <ethanstrominger2@gmail.com>
2021-04-30 18:39:20 -0400

    feat: CHH FE - WIP list-add remove func

--- covidhelphub-frontend/src/components/program/ProgramListComponent.js
+++ covidhelphub-frontend/src/components/program/ProgramListComponent.js
@@ -1,55 +1,102 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-//import ProgramService from '../services/ProgramService';
-import { useAppContext } from '../../AppContext';
+import React from 'react';
+import { render } from 'react-dom';
 
-const ProgramListComponent = () => {
-  // NOTE on React Hook: useState is used by React Hooks to create state variables and
-  // setter function for component
-  const { listPrograms } = useAppContext();
+export default class DynamicTable extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const [programs, setPrograms] = useState([]);
+    this.state = {
+      message: '',
+      items: [],
+    };
+  }
 
-  // ==== Fetch Programs into Programs variable ===
-  // NOTE on React Hook: useEffect: syntax by React Hooks. Code is called when component mounts,
-  // or is updated
-  useEffect(() => {
-    async function getData() {
-      const programs = listPrograms();
-      setPrograms(programs);
-    }
-    getData();
-  }, [programs]);
+  // updateMessage(event) {
+  //   this.setState({
+  //     message: event.target.value,
+  //   });
+  // }
 
-  const ProgramLinks = programs.map(program => (
-    <tr>
-      <td>
-        <Link
-          key={`Program${program}`}
-          to={{ pathname: '/program/display', name: program.code }}
-        >
-          {program.code}
-        </Link>
-      </td>
-    </tr>
-  ));
+  // handleClick() {
+  //   var items = this.state.items;
 
-  // === return fragment with a h4 and the list of Programs.
-  return (
-    <div>
-      <header className="list-header">
-        <h4>Programs</h4>
-        <Link className="list-btn" to="/program/create">
-          <button>Add</button>
-        </Link>
-      </header>
-      <table>
-        <tr>
-          <th>Name</th>
+  //   items.push(this.state.message);
+
+  //   this.setState({
+  //     items: items,
+  //     message: '',
+  //   });
+  // }
+
+  // handleItemChanged(i, event) {
+  //   var items = this.state.items;
+  //   items[i] = event.target.value;
+
+  //   this.setState({
+  //     items: items,
+  //   });
+  // }
+
+  // handleItemDeleted(i) {
+  //   var items = this.state.items;
+
+  //   items.splice(i, 1);
+
+  //   this.setState({
+  //     items: items,
+  //   });
+  // }
+
+  renderRows() {
+    var context = this;
+
+    return this.state.items.map(function(o, i) {
+      return (
+        <tr key={'item-' + i}>
+          <td>
+            <input
+              type="text"
+              value={o}
+              // onChange={context.handleItemChanged.bind(context, i)}
+            />
+          </td>
+          <td>
+            <button
+            // onClick={context.handleItemDeleted.bind(context, i)}
+            >
+              Delete
+            </button>
+          </td>
         </tr>
-        {ProgramLinks}
-      </table>
-    </div>
-  );
-};
-export default ProgramListComponent;
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <table className="">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderRows()}</tbody>
+        </table>
+        <hr />
+        <input
+          type="text"
+          value={this.state.message}
+          // onChange={this.updateMessage.bind(this)}
+        />
+        <button
+        //onClick={this.handleClick.bind(this)}
+        >
+          Add Item
+        </button>
+      </div>
+    );
+  }
+}
+render(<DynamicTable />, document.getElementById('root'));
