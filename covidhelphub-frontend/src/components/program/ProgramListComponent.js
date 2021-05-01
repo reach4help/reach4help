f9fd0f2dcbf5b631c35a27f2c3619544c0eedfa4
ethanstrominger <ethanstrominger2@gmail.com>
2021-05-01 10:31:30 -0400

    feat: covid fix delete bug

--- covidhelphub-frontend/src/components/program/ProgramListComponent.js
+++ covidhelphub-frontend/src/components/program/ProgramListComponent.js
@@ -6,6 +6,7 @@ const ProgramListComponent = () => {
   const [programs, setPrograms] = useState([]);
   const [newProgramCode, setNewProgramCode] = useState('');
   const [programCount, setProgramCount] = useState(0);
+  const [deleted, setDeleted] = useState(false);
 
   useEffect(() => {
     async function getData() {
@@ -16,20 +17,29 @@ const ProgramListComponent = () => {
     getData();
   }, []);
 
-  const ProgramLinks = programs.map((program, i) => (
-    <tr>
-      <td>
-        <input
-          type="text"
-          defaultValue={program.code}
-          onChange={e => updateArrayRow(e, i)}
-        />
-      </td>
-      <td>
-        <button onClick={() => deleteArrayRow(i)}>Delete</button>
-      </td>
-    </tr>
-  ));
+  let ProgramLinks = {};
+  console.log('figuring out program links', programs);
+  // if (deleted) {
+  //   ProgramLinks = <p>Deleted</p>;
+  // } else {
+  ProgramLinks = programs.map((program, i) => {
+    console.log('adding to link', program.code);
+    return (
+      <tr key={`item-${program.code}-${i}`}>
+        <td>
+          <input
+            type="text"
+            defaultValue={program.code}
+            onChange={e => updateArrayRow(e, i)}
+          />
+        </td>
+        <td>
+          <button onClick={e => deleteArrayRow(i)}>Delete</button>
+        </td>
+      </tr>
+    );
+  });
+  // }
 
   function addProgramToArray() {
     programs.push(new Program(newProgramCode));
@@ -39,9 +49,15 @@ const ProgramListComponent = () => {
   }
 
   function deleteArrayRow(i) {
+    console.log('deleting', i);
+    console.log('debug 1', programs);
+    console.log('debug 2', programs[i]);
     programs.splice(i, 1);
+    console.log('debug 3', programs);
+    console.log('debug 4', programs[i]);
     setPrograms(programs);
     setProgramCount(programs.length);
+    setDeleted(true);
   }
 
   function refreshNewProgramCode(e) {
@@ -62,11 +78,15 @@ const ProgramListComponent = () => {
     setPrograms(programs);
   }
 
+  async function refreshScreen() {
+    setDeleted(false);
+  }
+
   async function savePrograms() {
     const newPrograms = await ProgramService.saveMany(programs);
     console.log('new programs', newPrograms);
   }
-
+  console.log('programs', programs, ProgramLinks);
   return (
     <div>
       <table className="">
@@ -82,13 +102,14 @@ const ProgramListComponent = () => {
       <div>
         <input
           type="text"
-          value={newProgramCode}
+          defaultValue={newProgramCode}
           onChange={e => refreshNewProgramCode(e)}
         />
         <button onClick={addProgramToArray}>Add Item</button>
       </div>
       <br />
       <button onClick={savePrograms}>Save</button>
+      <button onClick={refreshScreen}>Refresh</button>
       <button onClick={revertPrograms}>Revert</button>
     </div>
   );
