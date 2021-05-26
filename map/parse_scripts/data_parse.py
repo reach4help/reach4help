bc38ac22ea58fadeafccefdac3661f3eb999e2d8
Shayan Chowdhury <shayan@reach4help.org>
2021-05-26 14:32:17 +0600

    feat: fully parsed Rajasthan, Madhya Pradesh, Maharashtra, Delhi and Uttar Pradesh

--- map/parse_scripts/data_parse.py
+++ map/parse_scripts/data_parse.py
@@ -101,15 +101,18 @@ def format_description(header, string):
 # data_values: List[String]
 # Converts data from a row into a dict of multiple items.
 # The number of items in the output corresponds to the number of nonempty categories in the row.
-def convert_item_to_dict(category, headers, city, state, data_values):
+def convert_item_to_dict(category, headers, state, city, data_values):
     header_map = {
         "Distributor Name": "Distributor Name",
         "Hospital Name": "Distributor Name",
         "Name": "Distributor Name",
         "Area": "Location",
+        "Contact Person": "Description",  # Manually fill back into appropriate field
+        "Contact Name": "Description",  # Manually fill back into appropriate field
         "Contact": "Distributor Contact (Phone)",
         "Contact ": "Distributor Contact (Phone)",
         "Mobile no.": "Distributor Contact (Phone)",
+        "Mobile No.": "Distributor Contact (Phone)",
         "Medicine name": "Description",
         "Cost per day": "Description",
         "Blood group": "Description",
@@ -143,11 +146,19 @@ def convert_item_to_dict(category, headers, city, state, data_values):
             if any(fluff in data for fluff in ["Oxygen", "Cylinder"]):
                 item_dict["Marked for Cleaning"] = True
 
+            geo_search_string = data
+            # If location is empty, try using the name (mark it as well to be sure)
+            if not data and "Distributor Name" in item_dict:
+                geo_search_string = item_dict["Distributor Name"]
+                item_dict["Marked for Cleaning"] = True
+
             # If the city isn't already included, add it (helps with getting the location via google)
             if item_dict["General Area (City)"] not in data:
-                geocode_result = gmaps.geocode(data + f" {city}", region="IN")
+                geocode_result = gmaps.geocode(
+                    geo_search_string + f" {city}", region="IN"
+                )
             else:
-                geocode_result = gmaps.geocode(data, region="IN")
+                geocode_result = gmaps.geocode(geo_search_string, region="IN")
 
             item_dict["lat"], item_dict["lng"] = extract_lat_lng(geocode_result)
 
@@ -169,6 +180,7 @@ def convert_item_to_dict(category, headers, city, state, data_values):
     item_dict["Description"] = " ; ".join(item_dict["Description"])
     return item_dict
 
+
 def extract_categories_and_headers(row):
     categories = [
         "Medicines",
@@ -185,27 +197,30 @@ def extract_categories_and_headers(row):
     cat_number = 0
 
     for header in row:
-            if header != "":
-                headers.append(header)
-            else:
-                categories_and_headers.append(
-                    [categories[cat_number], headers])
+
+        if header != "":
+            headers.append(header)
+        else:
+
+            categories_and_headers.append([categories[cat_number], headers])
+            if cat_number < len(categories) - 1:
                 cat_number += 1
-                headers = []
+            headers = []
 
     categories_and_headers.append([categories[cat_number], headers])
     return categories_and_headers
 
-def main(argv): 
-    inputfolder = ''
+
+def main(argv):
+    inputfolder = ""
     try:
-        opts, args = getopt.getopt(argv,"hi:",["ifolder="])
+        opts, args = getopt.getopt(argv, "hi:", ["ifolder="])
     except getopt.GetoptError:
-        print('test.py -i <inputfolder>')
+        print("test.py -i <inputfolder>")
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfolder>')
+        if opt == "-h":
+            print("test.py -i <inputfolder>")
             sys.exit()
         elif opt in ("-i", "--ifolder"):
             inputfolder = arg
@@ -215,13 +230,16 @@ def main(argv):
     output_dir = f"parsed_output_{current_time}"
     os.mkdir(output_dir)
 
-    for file in os.listdir(inputfolder):
+    input_dir_files = sorted(os.listdir(inputfolder))
+    print(input_dir_files)
+    for file in input_dir_files:
         i = 0
         json_i = 0
         final_dict = {}
 
-        naive_city_name, file_ext = file.split('.')[0], file.split('.')[1]
-        if file_ext == 'xls' or file_ext == 'xlsx':
+        naive_name, file_ext = file.split(".")
+        naive_state_name, naive_city_name = naive_name.split(" - ")
+        if file_ext == "xls" or file_ext == "xlsx":
             xls = pd.ExcelFile(f"{inputfolder}/{file}")
             sheet_names = xls.sheet_names
             sheet_to_df_map = {}
@@ -230,10 +248,10 @@ def main(argv):
             # *** This parsing section is incomplete ****
 
         else:
-            with open(f"{inputfolder}/{file}", encoding="utf-8") as csvfile:
+            with open(f"{inputfolder}/{file}", errors="ignore") as csvfile:
                 datareader = csv.reader(csvfile)
                 print(f"Starting parse for {file}...")
-                
+
                 for row in datareader:
                     i += 1
 
@@ -246,12 +264,18 @@ def main(argv):
                             num_of_headers = len(category_pair[1])
                             data_values = []
 
-                            for num in range(starting_column, starting_column + num_of_headers):
+                            for num in range(
+                                starting_column, starting_column + num_of_headers
+                            ):
                                 data_values.append(row[num])
 
                             if "".join(data_values) != "":
                                 item_dict = convert_item_to_dict(
-                                    category_pair[0], category_pair[1], naive_city_name, naive_city_name, data_values
+                                    category_pair[0],
+                                    category_pair[1],
+                                    naive_state_name,
+                                    naive_city_name,
+                                    data_values,
                                 )
                                 final_dict[json_i] = item_dict
                                 json_i += 1
@@ -262,15 +286,19 @@ def main(argv):
                     if i % 100 == 0:
                         print("Parsed " + str(i) + " rows. Still parsing...")
 
-
             # Export clean data to separate file
             # json.dump(final_dict, open("delhi_clean.json", "w+"), sort_keys=True, indent=4)
-            #     
+            #
             # Export clean data to csv for now (easier to look at quickly)
-            output_file_name = f"{output_dir}/{naive_city_name}_clean.csv"
-            pd.DataFrame.from_dict(final_dict).transpose().to_csv(output_file_name, index=False)
+            output_file_name = (
+                f"{output_dir}/{naive_state_name}_{naive_city_name}_clean.csv"
+            )
+            pd.DataFrame.from_dict(final_dict).transpose().to_csv(
+                output_file_name, index=False
+            )
             print(f"Output stored in {output_file_name}")
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
+
