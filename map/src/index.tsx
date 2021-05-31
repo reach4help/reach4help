20713fd27fd0a4bbe62ae5e31513eb719f078055
Shayan Chowdhury <shayan@reach4help.org>
2021-05-31 12:16:48 +0600

    feat: add Google Analytics pageview tracking

--- map/src/index.tsx
+++ map/src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TagManager from 'react-gtm-module';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createGlobalStyle, THEME, ThemeProvider } from './styling';
-
-TagManager.initialize({ gtmId: 'GTM-NGVT3FS' });
 
 const GlobalStyle = createGlobalStyle`
   body {
