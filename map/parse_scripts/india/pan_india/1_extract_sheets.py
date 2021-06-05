f6caa8728bda1f66c38beba55bb2c9e5530b820a
Shayan Chowdhury <shayan@reach4help.org>
2021-06-05 21:47:54 +0600

    chore: organize scripts and instructions for running

--- map/parse_scripts/india/pan_india/1_extract_sheets.py
+++ map/parse_scripts/india/pan_india/1_extract_sheets.py
@@ -4,24 +4,26 @@ import os
 import csv
 
 # list files
-path = os.getcwd()
+path = "./_raw_data"
 files = os.listdir(path)
 print(files)
 
 # Pick out 'xlsx' files
-files_xlsx = [f for f in files if f[-4:] == 'xlsx']
+files_xlsx = [f for f in files if f[-4:] == "xlsx"]
 print(files_xlsx)
 
 # iterate over each xlsx file in the directory and then over worksheets
 for file in files_xlsx:
-    wb = openpyxl.load_workbook(file)
+    wb = openpyxl.load_workbook(os.path.join(path, file))
     print(wb.sheetnames)
     sheetnames = wb.sheetnames[1:]  # skipping the first sheet
     for sheetname in sheetnames:
 
         sheet = wb[sheetname]
-        csvfilename = "input_files" + file[:-5] + "-" + sheetname + ".csv"
-        col = csv.writer(open(csvfilename, 'w', newline=""))
+        csvfilename = os.path.join(
+            "input_files", file[:-5] + " - " + sheetname + ".csv"
+        )
+        col = csv.writer(open(csvfilename, "w", newline=""))
         for r in sheet.rows:
             # row by row write
             col.writerow([cell.value for cell in r])
