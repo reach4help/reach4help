7301ca684546c1ac49ecf2de95bfd40aefff94e6
Sebastián Sameghini <ssameghini@users.noreply.github.com>
2021-06-05 19:29:48 -0300

    Correct typo in color attribute
    Some selectors had "colors" instead of "color" as an attribute, deriving in those styles not being aplied.

--- map/src/data/index.ts
+++ map/src/data/index.ts
@@ -8,26 +8,26 @@ export const SERVICES = {
     color: COLORS.orange,
   },
   beds: {
-    colors: COLORS.red,
+    color: COLORS.red,
   },
   oxygen: {
-    colors: COLORS.red,
+    color: COLORS.red,
   },
   medicine: {
     color: COLORS.purple,
   },
   'home-care': {
-    colors: COLORS.orange,
+    color: COLORS.orange,
   },
   blood: {
-    colors: COLORS.red,
+    color: COLORS.red,
   },
   quarantine: {
-    colors: COLORS.gray,
+    color: COLORS.gray,
   },
   telehealth: {
     // TODO: Might need to rename this?
-    colors: COLORS.blue,
+    color: COLORS.blue,
   },
   financial: {
     // TODO: rename to donations
